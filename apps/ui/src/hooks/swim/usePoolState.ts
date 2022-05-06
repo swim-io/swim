6182ff21c6b5efc93266cc44ffc56c3aa70c2720
wormat <wormat@maelstrom.finance>
2022-05-06 19:27:31 -0400

    Add plural versions of usePoolLpMint and usePoolState hooks

--- apps/ui/src/hooks/swim/usePoolState.ts
+++ apps/ui/src/hooks/swim/usePoolState.ts
@@ -1,10 +1,24 @@
 import type { UseQueryResult } from "react-query";
-import { useQuery } from "react-query";
+import { useQueries, useQuery } from "react-query";
 
 import type { PoolSpec } from "../../config";
 import { useEnvironment, useSolanaConnection } from "../../contexts";
 import type { SwimPoolState } from "../../models";
 import { getPoolState } from "../../models";
+
+export const usePoolStates = (
+  poolSpecs: readonly PoolSpec[],
+): readonly UseQueryResult<SwimPoolState | null, Error>[] => {
+  const { env } = useEnvironment();
+  const solanaConnection = useSolanaConnection();
+
+  return useQueries(
+    poolSpecs.map((poolSpec) => ({
+      queryKey: ["poolState", env, poolSpec.id],
+      queryFn: async () => getPoolState(solanaConnection, poolSpec),
+    })),
+  ) as readonly UseQueryResult<SwimPoolState | null, Error>[];
+};
 
 export const usePoolState = (
   poolSpec: PoolSpec,

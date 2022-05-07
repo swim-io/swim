719a3cd4a0e22709a790bfff8ea8fb8826454505
wormat <wormat@maelstrom.finance>
2022-05-07 14:13:45 -0400

    Comment out storeInteraction for now

--- apps/ui/src/models/swim/storeInteraction.ts
+++ apps/ui/src/models/swim/storeInteraction.ts
@@ -14,7 +14,6 @@ import type {
   RemoveExactOutputInteraction,
   RemoveUniformInteraction,
   SwapInteraction,
-  SwimInteraction,
 } from "./interaction";
 import { getTokensByPool } from "./pool";
 
@@ -26,227 +25,232 @@ const VERSION = 1;
 
 const getStorageKey = (env: Env): string => `interactions:${env}`;
 
-export interface PreparedAddInteraction extends SwimInteraction {
-  readonly instruction: SwimDefiInstruction.Add;
-  readonly params: {
-    readonly inputAmounts: readonly string[];
-    readonly minimumMintAmount: string;
-  };
-  readonly lpTokenTargetEcosystem: EcosystemId;
-}
+// export interface PreparedAddInteraction extends AddInteraction {
+//   readonly instruction: SwimDefiInstruction.Add;
+//   readonly params: {
+//     readonly inputAmounts: readonly string[];
+//     readonly minimumMintAmount: string;
+//   };
+//   readonly lpTokenTargetEcosystem: EcosystemId;
+// }
 
-export interface PreparedSwapInteraction extends SwimInteraction {
-  readonly instruction: SwimDefiInstruction.Swap;
-  readonly params: {
-    readonly exactInputAmounts: readonly string[];
-    readonly outputTokenIndex: number;
-    readonly minimumOutputAmount: string;
-  };
-}
+// export interface PreparedSwapInteraction extends SwapInteraction {
+//   readonly instruction: SwimDefiInstruction.Swap;
+//   readonly params: {
+//     readonly exactInputAmounts: readonly string[];
+//     readonly outputTokenIndex: number;
+//     readonly minimumOutputAmount: string;
+//   };
+// }
 
-export interface PreparedRemoveUniformInteraction extends SwimInteraction {
-  readonly instruction: SwimDefiInstruction.RemoveUniform;
-  readonly params: {
-    readonly exactBurnAmount: string;
-    readonly minimumOutputAmounts: readonly string[];
-  };
-  readonly lpTokenSourceEcosystem: EcosystemId;
-}
+// export interface PreparedRemoveUniformInteraction
+//   extends RemoveUniformInteraction {
+//   readonly instruction: SwimDefiInstruction.RemoveUniform;
+//   readonly params: {
+//     readonly exactBurnAmount: string;
+//     readonly minimumOutputAmounts: readonly string[];
+//   };
+//   readonly lpTokenSourceEcosystem: EcosystemId;
+// }
 
-export interface PreparedRemoveExactBurnInteraction extends SwimInteraction {
-  readonly instruction: SwimDefiInstruction.RemoveExactBurn;
-  readonly params: {
-    readonly exactBurnAmount: string;
-    readonly outputTokenIndex: number;
-    readonly minimumOutputAmount: string;
-  };
-  readonly lpTokenSourceEcosystem: EcosystemId;
-}
+// export interface PreparedRemoveExactBurnInteraction
+//   extends RemoveExactBurnInteraction {
+//   readonly instruction: SwimDefiInstruction.RemoveExactBurn;
+//   readonly params: {
+//     readonly exactBurnAmount: string;
+//     readonly outputTokenIndex: number;
+//     readonly minimumOutputAmount: string;
+//   };
+//   readonly lpTokenSourceEcosystem: EcosystemId;
+// }
 
-export interface PreparedRemoveExactOutputInteraction extends SwimInteraction {
-  readonly instruction: SwimDefiInstruction.RemoveExactOutput;
-  readonly params: {
-    readonly maximumBurnAmount: string;
-    readonly exactOutputAmounts: readonly string[];
-  };
-  readonly lpTokenSourceEcosystem: EcosystemId;
-}
+// export interface PreparedRemoveExactOutputInteraction
+//   extends RemoveExactOutputInteraction {
+//   readonly instruction: SwimDefiInstruction.RemoveExactOutput;
+//   readonly params: {
+//     readonly maximumBurnAmount: string;
+//     readonly exactOutputAmounts: readonly string[];
+//   };
+//   readonly lpTokenSourceEcosystem: EcosystemId;
+// }
 
-export type PreparedInteraction = {
-  readonly version: number;
-} & (
-  | PreparedAddInteraction
-  | PreparedSwapInteraction
-  | PreparedRemoveUniformInteraction
-  | PreparedRemoveExactBurnInteraction
-  | PreparedRemoveExactOutputInteraction
-);
+// export type PreparedInteraction = {
+//   readonly version: number;
+// } & (
+//   | PreparedAddInteraction
+//   | PreparedSwapInteraction
+//   | PreparedRemoveUniformInteraction
+//   | PreparedRemoveExactBurnInteraction
+//   | PreparedRemoveExactOutputInteraction
+// );
 
-const populateAddInteraction = (
-  tokens: readonly TokenSpec[],
-  lpToken: TokenSpec,
-  interaction: PreparedAddInteraction,
-): AddInteraction => {
-  const { env } = interaction;
-  if (!isValidEnv(env)) {
-    throw new Error("Invalid env");
-  }
-  return {
-    ...interaction,
-    env,
-    params: {
-      ...interaction.params,
-      inputAmounts: interaction.params.inputAmounts.map((amount, i) =>
-        Amount.fromHumanString(tokens[i], amount),
-      ),
-      minimumMintAmount: Amount.fromHumanString(
-        lpToken,
-        interaction.params.minimumMintAmount,
-      ),
-    },
-  };
-};
+// const populateAddInteraction = (
+//   tokens: readonly TokenSpec[],
+//   lpToken: TokenSpec,
+//   interaction: PreparedAddInteraction,
+// ): AddInteraction => {
+//   const { env } = interaction;
+//   if (!isValidEnv(env)) {
+//     throw new Error("Invalid env");
+//   }
+//   return {
+//     ...interaction,
+//     env,
+//     params: {
+//       ...interaction.params,
+//       inputAmounts: interaction.params.inputAmounts.map((amount, i) =>
+//         Amount.fromHumanString(tokens[i], amount),
+//       ),
+//       minimumMintAmount: Amount.fromHumanString(
+//         lpToken,
+//         interaction.params.minimumMintAmount,
+//       ),
+//     },
+//   };
+// };
 
-const populateSwapInteraction = (
-  tokens: readonly TokenSpec[],
-  interaction: PreparedSwapInteraction,
-): SwapInteraction => {
-  const { env } = interaction;
-  if (!isValidEnv(env)) {
-    throw new Error("Invalid env");
-  }
-  return {
-    ...interaction,
-    env,
-    params: {
-      ...interaction.params,
-      exactInputAmounts: interaction.params.exactInputAmounts.map((amount, i) =>
-        Amount.fromHumanString(tokens[i], amount),
-      ),
-      minimumOutputAmount: Amount.fromHumanString(
-        tokens[interaction.params.outputTokenIndex],
-        interaction.params.minimumOutputAmount,
-      ),
-    },
-  };
-};
+// const populateSwapInteraction = (
+//   tokens: readonly TokenSpec[],
+//   interaction: PreparedSwapInteraction,
+// ): SwapInteraction => {
+//   const { env } = interaction;
+//   if (!isValidEnv(env)) {
+//     throw new Error("Invalid env");
+//   }
+//   return {
+//     ...interaction,
+//     env,
+//     params: {
+//       ...interaction.params,
+//       exactInputAmounts: interaction.params.exactInputAmounts.map((amount, i) =>
+//         Amount.fromHumanString(tokens[i], amount),
+//       ),
+//       minimumOutputAmount: Amount.fromHumanString(
+//         tokens[interaction.params.outputTokenIndex],
+//         interaction.params.minimumOutputAmount,
+//       ),
+//     },
+//   };
+// };
 
-const populateRemoveUniformInteraction = (
-  tokens: readonly TokenSpec[],
-  lpToken: TokenSpec,
-  interaction: PreparedRemoveUniformInteraction,
-): RemoveUniformInteraction => {
-  const { env } = interaction;
-  if (!isValidEnv(env)) {
-    throw new Error("Invalid env");
-  }
-  return {
-    ...interaction,
-    env,
-    params: {
-      ...interaction.params,
-      exactBurnAmount: Amount.fromHumanString(
-        lpToken,
-        interaction.params.exactBurnAmount,
-      ),
-      minimumOutputAmounts: interaction.params.minimumOutputAmounts.map(
-        (amount, i) => Amount.fromHumanString(tokens[i], amount),
-      ),
-    },
-  };
-};
+// const populateRemoveUniformInteraction = (
+//   tokens: readonly TokenSpec[],
+//   lpToken: TokenSpec,
+//   interaction: PreparedRemoveUniformInteraction,
+// ): RemoveUniformInteraction => {
+//   const { env } = interaction;
+//   if (!isValidEnv(env)) {
+//     throw new Error("Invalid env");
+//   }
+//   return {
+//     ...interaction,
+//     env,
+//     params: {
+//       ...interaction.params,
+//       exactBurnAmount: Amount.fromHumanString(
+//         lpToken,
+//         interaction.params.exactBurnAmount,
+//       ),
+//       minimumOutputAmounts: interaction.params.minimumOutputAmounts.map(
+//         (amount, i) => Amount.fromHumanString(tokens[i], amount),
+//       ),
+//     },
+//   };
+// };
 
-const populateRemoveExactBurnInteraction = (
-  tokens: readonly TokenSpec[],
-  lpToken: TokenSpec,
-  interaction: PreparedRemoveExactBurnInteraction,
-): RemoveExactBurnInteraction => {
-  const { env } = interaction;
-  if (!isValidEnv(env)) {
-    throw new Error("Invalid env");
-  }
-  return {
-    ...interaction,
-    env,
-    params: {
-      ...interaction.params,
-      exactBurnAmount: Amount.fromHumanString(
-        lpToken,
-        interaction.params.exactBurnAmount,
-      ),
-      minimumOutputAmount: Amount.fromHumanString(
-        tokens[interaction.params.outputTokenIndex],
-        interaction.params.minimumOutputAmount,
-      ),
-    },
-  };
-};
+// const populateRemoveExactBurnInteraction = (
+//   tokens: readonly TokenSpec[],
+//   lpToken: TokenSpec,
+//   interaction: PreparedRemoveExactBurnInteraction,
+// ): RemoveExactBurnInteraction => {
+//   const { env } = interaction;
+//   if (!isValidEnv(env)) {
+//     throw new Error("Invalid env");
+//   }
+//   return {
+//     ...interaction,
+//     env,
+//     params: {
+//       ...interaction.params,
+//       exactBurnAmount: Amount.fromHumanString(
+//         lpToken,
+//         interaction.params.exactBurnAmount,
+//       ),
+//       minimumOutputAmount: Amount.fromHumanString(
+//         tokens[interaction.params.outputTokenIndex],
+//         interaction.params.minimumOutputAmount,
+//       ),
+//     },
+//   };
+// };
 
-const populateRemoveExactOutputInteraction = (
-  tokens: readonly TokenSpec[],
-  lpToken: TokenSpec,
-  interaction: PreparedRemoveExactOutputInteraction,
-): RemoveExactOutputInteraction => {
-  const { env } = interaction;
-  if (!isValidEnv(env)) {
-    throw new Error("Invalid env");
-  }
-  return {
-    ...interaction,
-    env,
-    params: {
-      ...interaction.params,
-      maximumBurnAmount: Amount.fromHumanString(
-        lpToken,
-        interaction.params.maximumBurnAmount,
-      ),
-      exactOutputAmounts: interaction.params.exactOutputAmounts.map(
-        (amount, i) => Amount.fromHumanString(tokens[i], amount),
-      ),
-    },
-  };
-};
+// const populateRemoveExactOutputInteraction = (
+//   tokens: readonly TokenSpec[],
+//   lpToken: TokenSpec,
+//   interaction: PreparedRemoveExactOutputInteraction,
+// ): RemoveExactOutputInteraction => {
+//   const { env } = interaction;
+//   if (!isValidEnv(env)) {
+//     throw new Error("Invalid env");
+//   }
+//   return {
+//     ...interaction,
+//     env,
+//     params: {
+//       ...interaction.params,
+//       maximumBurnAmount: Amount.fromHumanString(
+//         lpToken,
+//         interaction.params.maximumBurnAmount,
+//       ),
+//       exactOutputAmounts: interaction.params.exactOutputAmounts.map(
+//         (amount, i) => Amount.fromHumanString(tokens[i], amount),
+//       ),
+//     },
+//   };
+// };
 
-const populateInteraction = (
-  tokens: readonly TokenSpec[],
-  lpToken: TokenSpec,
-  interaction: PreparedInteraction,
-): Interaction => {
-  switch (interaction.instruction) {
-    case SwimDefiInstruction.Add:
-      return populateAddInteraction(tokens, lpToken, interaction);
-    case SwimDefiInstruction.Swap:
-      return populateSwapInteraction(tokens, interaction);
-    case SwimDefiInstruction.RemoveUniform:
-      return populateRemoveUniformInteraction(tokens, lpToken, interaction);
-    case SwimDefiInstruction.RemoveExactBurn:
-      return populateRemoveExactBurnInteraction(tokens, lpToken, interaction);
-    case SwimDefiInstruction.RemoveExactOutput:
-      return populateRemoveExactOutputInteraction(tokens, lpToken, interaction);
-    default:
-      throw new Error("Interaction not recognized");
-  }
-};
+// const populateInteraction = (
+//   tokens: readonly TokenSpec[],
+//   lpToken: TokenSpec,
+//   interaction: PreparedInteraction,
+// ): Interaction => {
+//   switch (interaction.instruction) {
+//     case SwimDefiInstruction.Add:
+//       return populateAddInteraction(tokens, lpToken, interaction);
+//     case SwimDefiInstruction.Swap:
+//       return populateSwapInteraction(tokens, interaction);
+//     case SwimDefiInstruction.RemoveUniform:
+//       return populateRemoveUniformInteraction(tokens, lpToken, interaction);
+//     case SwimDefiInstruction.RemoveExactBurn:
+//       return populateRemoveExactBurnInteraction(tokens, lpToken, interaction);
+//     case SwimDefiInstruction.RemoveExactOutput:
+//       return populateRemoveExactOutputInteraction(tokens, lpToken, interaction);
+//     default:
+//       throw new Error("Interaction not recognized");
+//   }
+// };
 
 const deserializeInteractions = (
   config: Config,
   serialized: string,
 ): readonly Interaction[] => {
-  try {
-    const tokensByPool = getTokensByPool(config);
-    const parsed: readonly PreparedInteraction[] = JSON.parse(serialized);
-    return filterMap(
-      (interaction: PreparedInteraction) => interaction.version === VERSION,
-      (interaction) => {
-        const { lpToken, tokens } = tokensByPool[interaction.poolId];
-        return populateInteraction(tokens, lpToken, interaction);
-      },
-      parsed,
-    );
-  } catch (err) {
-    Sentry.captureException(err);
-    return [];
-  }
+  // TODO:
+  return [];
+  // try {
+  //   const tokensByPool = getTokensByPool(config);
+  //   const parsed: readonly PreparedInteraction[] = JSON.parse(serialized);
+  //   return filterMap(
+  //     (interaction: PreparedInteraction) => interaction.version === VERSION,
+  //     (interaction) => {
+  //       const { lpToken, tokens } = tokensByPool[interaction.poolId];
+  //       return populateInteraction(tokens, lpToken, interaction);
+  //     },
+  //     parsed,
+  //   );
+  // } catch (err) {
+  //   Sentry.captureException(err);
+  //   return [];
+  // }
 };
 
 export const loadInteractions = (

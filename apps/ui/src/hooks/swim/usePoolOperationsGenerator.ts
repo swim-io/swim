763d54219288eb16963a33c0d518ab8e1289ee0c
wormat <wormat@maelstrom.finance>
2022-05-08 21:37:28 -0400

    Switch pool operations generator to 1 tx per operation

--- apps/ui/src/hooks/swim/usePoolOperationsGenerator.ts
+++ apps/ui/src/hooks/swim/usePoolOperationsGenerator.ts
@@ -1,6 +1,6 @@
 import type { AccountInfo as TokenAccount } from "@solana/spl-token";
-import type { TransactionInstruction } from "@solana/web3.js";
-import { Keypair, Transaction } from "@solana/web3.js";
+import type { ParsedTransactionWithMeta } from "@solana/web3.js";
+import type Decimal from "decimal.js";
 
 import type { Env, PoolSpec } from "../../config";
 import { EcosystemId, getSolanaTokenDetails } from "../../config";
@@ -20,10 +20,12 @@ import type {
   WithSplTokenAccounts,
 } from "../../models";
 import {
+  Amount,
   SwimDefiInstruction,
   SwimDefiInstructor,
-  createMemoIx,
   findTokenAccountForMint,
+  getAmountMintedToAccountByMint,
+  getAmountTransferredToAccountByMint,
   getPoolState,
   getRequiredPools,
   getTokensByPool,
@@ -99,84 +101,123 @@ const doSinglePoolOperation = async (
   }
 };
 
-const createPoolIxs = async (
-  env: Env,
-  solanaConnection: SolanaConnection,
-  wallet: SolanaWalletAdapter,
+const getTransferredAmount = (
+  inputOperationSpec: OperationSpec,
+  mintAddress: string,
+  walletAddress: string,
   splTokenAccounts: readonly TokenAccount[],
-  tokensByPoolId: TokensByPoolId,
-  poolSpec: PoolSpec,
-  operation: OperationSpec,
-  userTransferAuthority: Keypair,
-): Promise<readonly TransactionInstruction[]> => {
-  const walletAddress = wallet.publicKey?.toBase58() ?? null;
+  tx: ParsedTransactionWithMeta,
+): Decimal =>
+  inputOperationSpec.instruction === SwimDefiInstruction.Add
+    ? getAmountMintedToAccountByMint(
+        splTokenAccounts,
+        tx,
+        mintAddress,
+        walletAddress,
+      )
+    : getAmountTransferredToAccountByMint(
+        splTokenAccounts,
+        tx,
+        mintAddress,
+        walletAddress,
+      );
+
+const setOutputOperationInputAmount = (
+  splTokenAccounts: readonly TokenAccount[],
+  interaction: Interaction,
+  inputOperationSpec: OperationSpec,
+  outputOperationSpec: OperationSpec,
+  tx: ParsedTransactionWithMeta,
+): OperationSpec => {
+  const walletAddress = interaction.connectedWallets[EcosystemId.Solana];
   if (walletAddress === null) {
     throw new Error("Missing Solana wallet");
   }
-  const poolState = await getPoolState(solanaConnection, poolSpec);
-  if (poolState === null) {
-    throw new Error("Missing pool state");
-  }
 
-  const poolTokens = tokensByPoolId[poolSpec.id];
-  const lpTokenMintAddress = getSolanaTokenDetails(poolTokens.lpToken).address;
-  const userLpAccount = findTokenAccountForMint(
-    lpTokenMintAddress,
-    walletAddress,
-    splTokenAccounts,
-  );
-  const tokenMintAddresses = poolTokens.tokens.map(
-    (token) => getSolanaTokenDetails(token).address,
-  );
-  const userTokenAccounts = tokenMintAddresses.map((mint) =>
-    findTokenAccountForMint(mint, walletAddress, splTokenAccounts),
-  );
-  const instructor = new SwimDefiInstructor(
-    env,
-    solanaConnection,
-    wallet,
-    poolSpec.contract,
-    poolSpec.address,
-    poolSpec.authority,
-    lpTokenMintAddress,
-    poolState.governanceFeeKey.toBase58(),
-    tokenMintAddresses,
-    [...poolSpec.tokenAccounts.values()],
-    userLpAccount?.address.toBase58(),
-    userTokenAccounts.map((t) => t?.address.toBase58() ?? null),
-  );
-  switch (operation.instruction) {
-    // TODO: Fill out
-    case SwimDefiInstruction.Add:
-      return instructor.createAllAddIxs(
-        operation,
-        userTransferAuthority,
-        false,
+  switch (outputOperationSpec.instruction) {
+    case SwimDefiInstruction.Add: {
+      const inputAmount = findOrThrow(
+        outputOperationSpec.params.inputAmounts,
+        (amount) => !amount.isZero(),
       );
+      const mintAddress = getSolanaTokenDetails(inputAmount.tokenSpec).address;
+      const decimalAmount = getTransferredAmount(
+        inputOperationSpec,
+        mintAddress,
+        walletAddress,
+        splTokenAccounts,
+        tx,
+      );
+      const newInputAmount = Amount.fromAtomic(
+        inputAmount.tokenSpec,
+        decimalAmount,
+        EcosystemId.Solana,
+      );
+      return {
+        ...outputOperationSpec,
+        params: {
+          ...outputOperationSpec.params,
+          inputAmounts: outputOperationSpec.params.inputAmounts.map((amount) =>
+            amount.tokenId === inputAmount.tokenId ? newInputAmount : amount,
+          ),
+        },
+      };
+    }
+    case SwimDefiInstruction.RemoveExactBurn: {
+      const inputAmount = outputOperationSpec.params.exactBurnAmount;
+      const mintAddress = getSolanaTokenDetails(inputAmount.tokenSpec).address;
+      const decimalAmount = getTransferredAmount(
+        inputOperationSpec,
+        mintAddress,
+        walletAddress,
+        splTokenAccounts,
+        tx,
+      );
+      const newInputAmount = Amount.fromAtomic(
+        inputAmount.tokenSpec,
+        decimalAmount,
+        EcosystemId.Solana,
+      );
+      return {
+        ...outputOperationSpec,
+        params: {
+          ...outputOperationSpec.params,
+          exactBurnAmount: newInputAmount,
+        },
+      };
+    }
+    case SwimDefiInstruction.Swap: {
+      const inputAmount = findOrThrow(
+        outputOperationSpec.params.exactInputAmounts,
+        (amount) => !amount.isZero(),
+      );
+      const mintAddress = getSolanaTokenDetails(inputAmount.tokenSpec).address;
+      const decimalAmount = getTransferredAmount(
+        inputOperationSpec,
+        mintAddress,
+        walletAddress,
+        splTokenAccounts,
+        tx,
+      );
+      const newInputAmount = Amount.fromAtomic(
+        inputAmount.tokenSpec,
+        decimalAmount,
+        EcosystemId.Solana,
+      );
+      return {
+        ...outputOperationSpec,
+        params: {
+          ...outputOperationSpec.params,
+          exactInputAmounts: outputOperationSpec.params.exactInputAmounts.map(
+            (amount) =>
+              amount.tokenId === inputAmount.tokenId ? newInputAmount : amount,
+          ),
+        },
+      };
+    }
+    // NOTE: Multi-operation swaps should not involve these instructions
     case SwimDefiInstruction.RemoveUniform:
-      return instructor.createAllRemoveUniformIxs(
-        operation,
-        userTransferAuthority,
-        false,
-      );
-    case SwimDefiInstruction.RemoveExactBurn:
-      return instructor.createAllRemoveExactBurnIxs(
-        operation,
-        userTransferAuthority,
-        false,
-      );
     case SwimDefiInstruction.RemoveExactOutput:
-      return instructor.createAllRemoveExactOutputIxs(
-        operation,
-        userTransferAuthority,
-        false,
-      );
-    case SwimDefiInstruction.Swap:
-      return instructor.createAllSwapIxs(
-        operation,
-        userTransferAuthority,
-        false,
-      );
     default:
       throw new Error("Unknown instruction");
   }
@@ -192,101 +233,64 @@ async function* generatePoolOperationTxs(
   interaction: Interaction,
   operations: readonly OperationSpec[],
 ): AsyncGenerator<SolanaTx> {
+  const inputOperation = operations[0];
+  const inputPoolSpec = findOrThrow(
+    poolSpecs,
+    (spec) => spec.id === inputOperation.poolId,
+  );
+  const inputTxId = await doSinglePoolOperation(
+    env,
+    solanaConnection,
+    wallet,
+    splTokenAccounts,
+    tokensByPoolId,
+    inputPoolSpec,
+    inputOperation,
+  );
+  const inputTx = await solanaConnection.getParsedTx(inputTxId);
+  yield {
+    ecosystem: EcosystemId.Solana,
+    txId: inputTxId,
+    timestamp: inputTx.blockTime ?? null,
+    interactionId: interaction.id,
+    parsedTx: inputTx,
+  };
+
   if (operations.length === 1) {
-    const [operation] = operations;
-    const poolSpec = findOrThrow(
-      poolSpecs,
-      (spec) => spec.id === operation.poolId,
-    );
-    const txId = await doSinglePoolOperation(
-      env,
-      solanaConnection,
-      wallet,
-      splTokenAccounts,
-      tokensByPoolId,
-      poolSpec,
-      operation,
-    );
-    const tx = await solanaConnection.getParsedTx(txId);
-    yield {
-      ecosystem: EcosystemId.Solana,
-      txId,
-      timestamp: tx.blockTime ?? null,
-      interactionId: interaction.id,
-      parsedTx: tx,
-    };
     return;
   }
   if (operations.length !== 2) {
     throw new Error("Unknown interaction route");
   }
 
-  // TODO: Refactor if this works
-  const userTransferAuthority = Keypair.generate();
-  const poolIxs = await Promise.all(
-    operations.map(async (operation) => {
-      const poolSpec = findOrThrow(
-        poolSpecs,
-        (spec) => spec.id === operation.poolId,
-      );
-      return createPoolIxs(
-        env,
-        solanaConnection,
-        wallet,
-        splTokenAccounts,
-        tokensByPoolId,
-        poolSpec,
-        operation,
-        userTransferAuthority,
-      );
-    }),
+  const outputOperation = setOutputOperationInputAmount(
+    splTokenAccounts,
+    interaction,
+    inputOperation,
+    operations[1],
+    inputTx,
   );
-  const memoIx = createMemoIx(interaction.id, []);
-  const tx = new Transaction({
-    feePayer: wallet.publicKey,
-  });
-  tx.add(...poolIxs.flat(), memoIx);
-  const signTransaction = async (
-    txToSign: Transaction,
-  ): Promise<Transaction> => {
-    txToSign.partialSign(userTransferAuthority);
-    return wallet.signTransaction(txToSign);
-  };
-  const txId = await solanaConnection.sendAndConfirmTx(signTransaction, tx);
-  const parsedTx = await solanaConnection.getParsedTx(txId);
+  const outputPoolSpec = findOrThrow(
+    poolSpecs,
+    (spec) => spec.id === outputOperation.poolId,
+  );
+  const outputTxId = await doSinglePoolOperation(
+    env,
+    solanaConnection,
+    wallet,
+    splTokenAccounts,
+    tokensByPoolId,
+    outputPoolSpec,
+    outputOperation,
+  );
+  const outputTx = await solanaConnection.getParsedTx(outputTxId);
   yield {
     ecosystem: EcosystemId.Solana,
-    txId,
-    timestamp: parsedTx.blockTime ?? null,
+    txId: outputTxId,
+    timestamp: outputTx.blockTime ?? null,
     interactionId: interaction.id,
-    parsedTx,
+    parsedTx: outputTx,
   };
-  return;
-
-  // TODO: Re-enable if multiple ixs per tx fails
-  // for (const operation of operations) {
-  //   const poolSpec = findOrThrow(
-  //     poolSpecs,
-  //     (spec) => spec.id === operation.poolId,
-  //   );
-  //   const txId = await doSinglePoolOperation(
-  //     env,
-  //     solanaConnection,
-  //     wallet,
-  //     splTokenAccounts,
-  //     tokensByPoolId,
-  //     poolSpec,
-  //     operation,
-  //   );
-  //   const tx = await solanaConnection.getParsedTx(txId);
-  //   yield {
-  //     ecosystem: EcosystemId.Solana,
-  //     txId,
-  //     timestamp: tx.blockTime ?? null,
-  //     interactionId: operation.interactionId,
-  //     parsedTx: tx,
-  //   };
-  // }
 }
 
 export interface PoolOperationsInput {

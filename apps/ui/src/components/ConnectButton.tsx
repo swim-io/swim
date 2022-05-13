0f2fa310119e5131b0839bcba15125b9b34798a6
wormat <wormat@maelstrom.finance>
2022-05-13 17:42:36 -0400

    Fix Avalanche/Polygon display on ConnectButton

--- apps/ui/src/components/ConnectButton.tsx
+++ apps/ui/src/components/ConnectButton.tsx
@@ -107,8 +107,8 @@ export const MultiConnectButton = ({
         {isSolanaConnected && <EuiIcon type={SOLANA_SVG} size="l" />}
         {isEthereumConnected && <EuiIcon type={ETHEREUM_SVG} size="l" />}
         {isBscConnected && <EuiIcon type={BSC_SVG} size="l" />}
-        <EuiIcon type={AVALANCHE_SVG} size="l" />
-        <EuiIcon type={POLYGON_SVG} size="l" />
+        {isAvalancheConnected && <EuiIcon type={AVALANCHE_SVG} size="l" />}
+        {isPolygonConnected && <EuiIcon type={POLYGON_SVG} size="l" />}
         &nbsp;{nConnected}
         <span>&nbsp;connected</span>
       </>

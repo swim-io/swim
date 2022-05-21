0e98e540a955ab84d7ab4e51fdbec46e4c65e7b6
Chiu <chiu@exsphere.com>
2022-05-21 13:04:20 +0800

    Add error hook

--- apps/ui/src/components/SwapForm.tsx
+++ apps/ui/src/components/SwapForm.tsx
@@ -10,12 +10,14 @@ import type Decimal from "decimal.js";
 import type { FormEvent, ReactElement, ReactNode } from "react";
 import { useEffect, useState } from "react";
 
-import { EcosystemId, ecosystems } from "../config";
+import { EcosystemId } from "../config";
 import { useConfig } from "../contexts";
 import { selectNotify } from "../core/selectors";
 import { useNotification } from "../core/store";
 import { captureAndWrapException } from "../errors";
 import {
+  useGetSwapFormErrors,
+  useIsLargeSwap,
   usePoolMaths,
   usePools,
   usePrevious,
@@ -25,7 +27,6 @@ import {
   useSwapOutputAmountEstimate,
   useUserBalanceAmounts,
   useUserNativeBalances,
-  useWallets,
 } from "../hooks";
 import {
   Amount,
@@ -34,18 +35,12 @@ import {
   getLowBalanceWallets,
   getRequiredPoolsForSwap,
 } from "../models";
-import {
-  defaultIfError,
-  findOrThrow,
-  isEachNotNull,
-  isNotNull,
-} from "../utils";
+import { defaultIfError, findOrThrow, isEachNotNull } from "../utils";
 
 import { ConfirmModal } from "./ConfirmModal";
 import { EstimatedTxFeesCallout } from "./EstimatedTxFeesCallout";
 import { LowBalanceDescription } from "./LowBalanceDescription";
 import { PoolPausedAlert } from "./PoolPausedAlert";
-import { isValidSlippageFraction } from "./SlippageButton";
 import { StepsDisplay } from "./StepsDisplay";
 import { NativeTokenIcon } from "./TokenIcon";
 import { SwapFormSolanaConnectButton } from "./molecules/SwapFormSolanaConnectButton";
@@ -65,7 +60,6 @@ export const SwapForm = ({
   const config = useConfig();
   const notify = useNotification(selectNotify);
   const { tokens } = config;
-  const wallets = useWallets();
   const { data: splTokenAccounts = null } = useSplTokenAccountsQuery();
   const userNativeBalances = useUserNativeBalances();
   const {
@@ -92,7 +86,6 @@ export const SwapForm = ({
   const [toTokenId, setToTokenId] = useState(swappableTokenIds[1]);
   const fromToken = findOrThrow(tokens, ({ id }) => id === fromTokenId);
   const toToken = findOrThrow(tokens, ({ id }) => id === toTokenId);
-
   const [formErrors, setFormErrors] = useState<readonly string[]>([]);
 
   const requiredPools = getRequiredPoolsForSwap(
@@ -102,15 +95,13 @@ export const SwapForm = ({
   );
   const poolIds = requiredPools.map((pool) => pool.id);
   const pools = usePools(poolIds);
-  const inputPoolUsdValue = pools[0].poolUsdValue;
-  const outputPoolUsdValue = pools[pools.length - 1].poolUsdValue;
   const isRequiredPoolPaused = pools.some((pool) => pool.isPoolPaused);
   const poolMaths = usePoolMaths(poolIds);
 
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const [isConfirmModalVisible, setIsConfirmModalVisible] = useState(false);
   const [confirmModalDescription, setConfirmModalDescription] =
-    useState<ReactNode>("");
+    useState<ReactNode | null>(null);
+  const isConfirmModalVisible = confirmModalDescription !== null;
 
   const [formInputAmount, setFormInputAmount] = useState("0");
   const [inputAmountErrors, setInputAmountErrors] = useState<readonly string[]>(
@@ -129,6 +120,21 @@ export const SwapForm = ({
   const inputAmount = defaultIfError(
     () => Amount.fromHumanString(fromToken, formInputAmount),
     Amount.zero(fromToken),
+  );
+
+  const isLargeSwap = useIsLargeSwap(fromToken, toToken, inputAmount);
+  const isSmallEthSwap =
+    fromToken.isStablecoin &&
+    [fromToken.nativeEcosystem, toToken.nativeEcosystem].includes(
+      EcosystemId.Ethereum,
+    ) &&
+    inputAmount.toHuman(EcosystemId.Solana).lt(200);
+
+  const getSwapFormErrors = useGetSwapFormErrors(
+    fromToken,
+    toToken,
+    inputAmount,
+    maxSlippageFraction,
   );
   const isInputAmountPositive =
     !inputAmount.isNegative() && !inputAmount.isZero();
@@ -169,36 +175,6 @@ export const SwapForm = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fromTokenBalancePrimitive]);
 
-  const isLargeSwap = (): boolean => {
-    return (
-      fromToken !== null &&
-      fromToken.isStablecoin &&
-      inputAmount !== null &&
-      ((inputPoolUsdValue !== null &&
-        inputAmount
-          .toHuman(EcosystemId.Solana)
-          .gt(inputPoolUsdValue.mul(0.1))) ||
-        (outputPoolUsdValue !== null &&
-          outputAmount !== null &&
-          outputAmount
-            .toHuman(EcosystemId.Solana)
-            .gt(outputPoolUsdValue.mul(0.1))))
-    );
-  };
-
-  const isSmallEthSwap = (): boolean => {
-    return (
-      fromToken !== null &&
-      fromToken.isStablecoin &&
-      toToken !== null &&
-      [fromToken.nativeEcosystem, toToken.nativeEcosystem].includes(
-        EcosystemId.Ethereum,
-      ) &&
-      inputAmount !== null &&
-      inputAmount.toHuman(EcosystemId.Solana).lt(200)
-    );
-  };
-
   const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
     const lowBalanceWallets = getLowBalanceWallets(
@@ -206,17 +182,14 @@ export const SwapForm = ({
       userNativeBalances,
     );
     if (lowBalanceWallets.length > 0) {
-      setIsConfirmModalVisible(true);
       setConfirmModalDescription(
         <LowBalanceDescription lowBalanceWallets={lowBalanceWallets} />,
       );
-    } else if (isLargeSwap()) {
-      setIsConfirmModalVisible(true);
+    } else if (isLargeSwap) {
       setConfirmModalDescription(
         "You're trying to swap >10% of the pool size which may impact the price.",
       );
-    } else if (isSmallEthSwap()) {
-      setIsConfirmModalVisible(true);
+    } else if (isSmallEthSwap) {
       setConfirmModalDescription(
         "The Ethereum gas fees might be high relative to the swap amount.",
       );
@@ -244,85 +217,10 @@ export const SwapForm = ({
   };
 
   const handleSwap = (allowLargeSwap: boolean): void => {
-    // reset everything
-    setFormErrors([]);
-    let errors: readonly string[] = [];
-
-    // Require connected Solana wallet
-    if (!wallets.solana.connected) {
-      errors = [...errors, "Connect Solana wallet"];
-    }
-
-    // Require source token to have a connected wallet
-    if (
-      fromToken.nativeEcosystem !== EcosystemId.Solana &&
-      !wallets[fromToken.nativeEcosystem].connected
-    ) {
-      errors = [
-        ...errors,
-        `Connect ${ecosystems[fromToken.nativeEcosystem].displayName} wallet`,
-      ];
-    }
-
-    // Require destination token to have a connected wallet
-    if (
-      toToken.nativeEcosystem !== EcosystemId.Solana &&
-      !wallets[toToken.nativeEcosystem].connected
-    ) {
-      errors = [
-        ...errors,
-        `Connect ${ecosystems[toToken.nativeEcosystem].displayName} wallet`,
-      ];
-    }
-
-    // Require non-zero native balances
-    const requiredEcosystems = new Set(
-      [
-        EcosystemId.Solana,
-        fromToken.nativeEcosystem,
-        toToken.nativeEcosystem,
-      ].filter(isNotNull),
-    );
-    requiredEcosystems.forEach((ecosystem) => {
-      if (userNativeBalances[ecosystem].isZero()) {
-        errors = [
-          ...errors,
-          `Empty balance in ${ecosystems[ecosystem].displayName} wallet. You will need some funds to pay for transaction fees.`,
-        ];
-      }
-    });
-
-    // Need some SOL for network fee
-    if (
-      userNativeBalances[EcosystemId.Solana].greaterThan(0) &&
-      userNativeBalances[EcosystemId.Solana].lessThan(0.01)
-    ) {
-      errors = [
-        ...errors,
-        `Low SOL in Solana wallet. You will need up to ~0.01 SOL to pay for network fees.`,
-      ];
-    }
-
-    // Require enough user balances
-    if (fromTokenBalance && inputAmount.gt(fromTokenBalance)) {
-      errors = [...errors, "Insufficient funds"];
-    }
-
-    if (inputAmount.isZero()) {
-      errors = [...errors, "Provide a valid amount"];
-    }
-
-    if (isLargeSwap() && !allowLargeSwap) {
-      // If not allowed, limit swap size to 10% of pool supply
-      errors = [...errors, "Swap size must be less than 10% of pool supply"];
-    }
-
-    if (!isValidSlippageFraction(maxSlippageFraction)) {
-      errors = [...errors, "Provide a valid max slippage setting"];
-    }
+    const errors = getSwapFormErrors(allowLargeSwap);
+    setFormErrors(errors);
 
     if (errors.length > 0) {
-      setFormErrors([...errors]);
       return;
     }
 
@@ -369,11 +267,11 @@ export const SwapForm = ({
   }, [toTokenId, toTokenOptions]);
 
   const handleConfirmModalCancel = (): void => {
-    setIsConfirmModalVisible(false);
+    setConfirmModalDescription(null);
   };
 
   const handleConfirmModalConfirm = async (): Promise<void> => {
-    setIsConfirmModalVisible(false);
+    setConfirmModalDescription(null);
     handleSwapAndCatch(true);
   };
 
---
+++ apps/ui/src/hooks/swim/useGetSwapFormErrors.ts
@@ -0,0 +1,105 @@
+import type Decimal from "decimal.js";
+
+import { isValidSlippageFraction } from "../../components/SlippageButton";
+import type { TokenSpec } from "../../config";
+import { EcosystemId, ecosystems } from "../../config";
+import type { Amount } from "../../models";
+import { isNotNull } from "../../utils";
+import {
+  useUserBalanceAmounts,
+  useUserNativeBalances,
+  useWallets,
+} from "../crossEcosystem";
+
+import { useIsLargeSwap } from "./useIsLargeSwap";
+
+export const useGetSwapFormErrors = (
+  fromToken: TokenSpec,
+  toToken: TokenSpec,
+  inputAmount: Amount,
+  maxSlippageFraction: Decimal | null,
+) => {
+  const wallets = useWallets();
+  const userNativeBalances = useUserNativeBalances();
+  const fromTokenUserBalances = useUserBalanceAmounts(fromToken);
+  const fromTokenBalance = fromTokenUserBalances[fromToken.nativeEcosystem];
+  const isLargeSwap = useIsLargeSwap(fromToken, toToken, inputAmount);
+
+  return (allowLargeSwap: boolean) => {
+    let errors: readonly string[] = [];
+    // Require connected Solana wallet
+    if (!wallets.solana.connected) {
+      errors = [...errors, "Connect Solana wallet"];
+    }
+
+    // Require source token to have a connected wallet
+    if (
+      fromToken.nativeEcosystem !== EcosystemId.Solana &&
+      !wallets[fromToken.nativeEcosystem].connected
+    ) {
+      errors = [
+        ...errors,
+        `Connect ${ecosystems[fromToken.nativeEcosystem].displayName} wallet`,
+      ];
+    }
+
+    // Require destination token to have a connected wallet
+    if (
+      toToken.nativeEcosystem !== EcosystemId.Solana &&
+      !wallets[toToken.nativeEcosystem].connected
+    ) {
+      errors = [
+        ...errors,
+        `Connect ${ecosystems[toToken.nativeEcosystem].displayName} wallet`,
+      ];
+    }
+
+    // Require non-zero native balances
+    const requiredEcosystems = new Set(
+      [
+        EcosystemId.Solana,
+        fromToken.nativeEcosystem,
+        toToken.nativeEcosystem,
+      ].filter(isNotNull),
+    );
+    requiredEcosystems.forEach((ecosystem) => {
+      if (userNativeBalances[ecosystem].isZero()) {
+        errors = [
+          ...errors,
+          `Empty balance in ${ecosystems[ecosystem].displayName} wallet. You will need some funds to pay for transaction fees.`,
+        ];
+      }
+    });
+
+    // Need some SOL for network fee
+    if (
+      userNativeBalances[EcosystemId.Solana].greaterThan(0) &&
+      userNativeBalances[EcosystemId.Solana].lessThan(0.01)
+    ) {
+      errors = [
+        ...errors,
+        `Low SOL in Solana wallet. You will need up to ~0.01 SOL to pay for network fees.`,
+      ];
+    }
+
+    // Require enough user balances
+    if (fromTokenBalance && inputAmount.gt(fromTokenBalance)) {
+      errors = [...errors, "Insufficient funds"];
+    }
+
+    if (inputAmount.isZero()) {
+      errors = [...errors, "Provide a valid amount"];
+    }
+
+    if (isLargeSwap && !allowLargeSwap) {
+      // If not allowed, limit swap size to 10% of pool supply
+      errors = [...errors, "Swap size must be less than 10% of pool supply"];
+    }
+
+    if (!isValidSlippageFraction(maxSlippageFraction)) {
+      errors = [...errors, "Provide a valid max slippage setting"];
+    }
+
+    return errors;
+  };
+};

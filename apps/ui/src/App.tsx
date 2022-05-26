8a44c17d5d927a105ae04108b0bfc88468a4b28c
page18 <andrew@kvasium.com>
2022-05-26 18:32:58 -0400

    Fix ottertot error logs and fix routing issues caused by env

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -73,15 +73,15 @@ function App(): ReactElement {
               <Route path="/swap">
                 <SwapPage />
               </Route>
+              {true && (
+                <Route exact path="/otter-tots">
+                  <OtterTotsPage />
+                </Route>
+              )}
               {process.env.REACT_APP_ENABLE_NFT && (
-                <>
-                  <Route exact path="/otter-tots">
-                    <OtterTotsPage />
-                  </Route>
-                  <Route path="/redeem">
-                    <RedeemPage />
-                  </Route>
-                </>
+                <Route path="/redeem">
+                  <RedeemPage />
+                </Route>
               )}
               <Route path="/collectibles">
                 <CollectiblesPage />

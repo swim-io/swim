e66e9c4f578678b10524a7763047b75b1a991644
swimdrew <95261047+swimdrew@users.noreply.github.com>
2022-06-02 14:36:58 -0400

    Move connection construction outside of interval and refactor useEffect() call.
    Co-authored-by: cypher <103903636+cyphertrace@users.noreply.github.com>

--- apps/ui/src/components/SolanaTpsWarning.tsx
+++ apps/ui/src/components/SolanaTpsWarning.tsx
@@ -16,48 +16,51 @@ export const SolanaTpsWarning = (): react.ReactElement | null => {
   const { chains } = useEnvironment(selectConfig, shallow);
   const [chain] = chains[Protocol.Solana];
   const { endpoint } = chain;
-  useEffect(() => {
-    let cancelled = false;
-    const interval = setInterval(() => {
-      void (async () => {
-        try {
-          // TODO: There is a bug with getRecentPerformanceSamples in which a new connection needs to be made.
-          // Should be fixed in subsequent Solana versions.
-          // https://github.com/solana-labs/solana/issues/19419
-          const connection = new Connection(endpoint);
-          const samples = await connection.getRecentPerformanceSamples(
-            SAMPLES_LIMIT,
-          );
-          if (samples.length >= 1) {
-            const stats = samples.reduce(
-              function ({ numTps, numSeconds }, sample) {
-                return {
-                  numTps: numTps + sample.numTransactions,
-                  numSeconds: numSeconds + sample.samplePeriodSecs,
-                };
-              },
-              { numTps: 0, numSeconds: 0 },
-            );
-            const avgTps = stats.numTps / stats.numSeconds;
-            if (!cancelled) {
-              setTps(avgTps);
-            }
-          }
-        } catch (e) {
-          // Do nothing with sampling error.
+  const connection = new Connection(endpoint); // We can create Connection here and just call it in interval
+
+  const checkSolanaTps = async () => {
+    try {
+      // TODO: There is a bug with getRecentPerformanceSamples in which a new connection needs to be made.
+      // Should be fixed in subsequent Solana versions.
+      // https://github.com/solana-labs/solana/issues/19419
+      const samples = await connection.getRecentPerformanceSamples(
+        SAMPLES_LIMIT,
+      );
+      if (samples.length >= 1) {
+        const stats = samples.reduce(
+          ({ numTps, numSeconds }, sample) => ({
+            numTps: numTps + sample.numTransactions,
+            numSeconds: numSeconds + sample.samplePeriodSecs,
+          }),
+          { numTps: 0, numSeconds: 0 },
+        );
+        const avgTps = stats.numTps / stats.numSeconds;
+        if (tps && avgTps >= 1500) {
+          setTps(null);
+        } else if (!tps && avgTps > 0) {
+          setTps(avgTps);
         }
-      })();
-    }, INTERVAL_FREQUENCY_MS);
+      }
+    } catch (e) {
+      // Do nothing with sampling error.
+      console.warn(e);
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(
+      async () => await checkSolanaTps(),
+      INTERVAL_FREQUENCY_MS,
+    );
+
+    checkSolanaTps().catch((err) => console.warn(err));
+
     return () => {
-      cancelled = true;
       clearInterval(interval);
     };
   }, []);
 
-  if (!tps || tps >= 1500) {
-    return null;
-  } else if (tps > 0) {
-    // TODO: Figure out what prompt should say.
+  if (tps && tps > 0) {
     return (
       <>
         <EuiCallOut title="Slow TPS" color="warning">
@@ -77,20 +80,20 @@ export const SolanaTpsWarning = (): react.ReactElement | null => {
         <EuiSpacer />
       </>
     );
-  } else {
-    return (
-      <>
-        <EuiCallOut title="Solana Network Down" color="danger">
-          <EuiText>
-            <p>
-              {"We've detected downtime on the"}
-              <a href="https://status.solana.com/">Solana Network </a>
-              {" and thus advise agaisnt swapping at this time."}
-            </p>
-          </EuiText>
-        </EuiCallOut>
-        <EuiSpacer />
-      </>
-    );
   }
+  return (
+    <>
+      <EuiCallOut title="Solana Network Down" color="danger">
+        <EuiText>
+          <p>
+            {"We've detected downtime on the"}
+            <a href="https://status.solana.com/">Solana Network </a>
+            {" and thus advise agaisnt swapping at this time."}
+          </p>
+        </EuiText>
+      </EuiCallOut>
+      <EuiSpacer />
+    </>
+  );
 };
+

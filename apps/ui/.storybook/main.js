59d366c3ce06c832dc8d4ea4b70a45ca3d5182c5
Nico Miicro <nico@swim.io>
2022-06-17 16:09:51 +0300

    chore(ui): format file with prettier

--- apps/ui/.storybook/main.js
+++ apps/ui/.storybook/main.js
@@ -1,9 +1,14 @@
-const webpack = require("webpack")
+const webpack = require("webpack");
 
 module.exports = {
   staticDirs: ["../public"],
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app",
+  ],
   framework: "@storybook/react",
   typescript: {
     check: false,
@@ -11,26 +16,27 @@ module.exports = {
     reactDocgen: "react-docgen-typescript",
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
-      propFilter: prop => prop.parent ? !/node_modules/.test(prop.parent.fileName) : true
-    }
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
   },
   core: {
     disableTelemetry: true,
-    builder: "webpack5"
+    builder: "webpack5",
   },
-  webpackFinal: async config => {
+  webpackFinal: async (config) => {
     // Verbose output from Webpack to help debugging on CI
-    config.stats = "verbose"
+    config.stats = "verbose";
 
     config.plugins.push(
       new webpack.ProvidePlugin({
         Buffer: ["buffer", "Buffer"],
-      })
-    )
+      }),
+    );
 
     config.externals = {
-      "@certusone/wormhole-sdk": "{}"
+      "@certusone/wormhole-sdk": "{}",
     };
     return config;
-  }
+  },
 };

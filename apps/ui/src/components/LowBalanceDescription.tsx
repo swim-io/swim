2861037f3ae331021d93379ee57d6d334f504924
swimdrew <andrew@kvasium.com>
2022-06-21 18:42:54 -0400

    fix: typos

--- apps/ui/src/components/LowBalanceDescription.tsx
+++ apps/ui/src/components/LowBalanceDescription.tsx
@@ -2,40 +2,40 @@ import type { FC, ReactElement } from "react";
 
 import { EcosystemId, ecosystems } from "../config";
 
-const EXSISTENTIAL_DEPOSIT_LINK =
+const EXISTENTIAL_DEPOSIT_LINK =
   "https://support.polkadot.network/support/solutions/articles/65000168651-what-is-the-existential-deposit";
 
 interface Props {
   readonly lowBalanceWallets: readonly EcosystemId[];
 }
 
-interface LowPolygonBalanceWarningProps {
+interface LowPolkadotBalanceWarningProps {
   readonly isVisible: boolean;
 }
 
-const LowPolygonBalanceWarning = ({
+const LowPolkadotBalanceWarning = ({
   isVisible,
-}: LowPolygonBalanceWarningProps): ReactElement | null => {
+}: LowPolkadotBalanceWarningProps): ReactElement | null => {
   if (!isVisible) {
     return null;
   }
   return (
     <span>
       <span>
-        *Polygon chains require a minimum balance in order not to be deactivated
-        according to &nbsp;
+        *Polkadot chains require a minimum balance in order not to be
+        deactivated according to
       </span>
-      <a href={EXSISTENTIAL_DEPOSIT_LINK}>Existential Deposit</a>
-      <span>&nbsp;requirements</span>
+      <a href={EXISTENTIAL_DEPOSIT_LINK}> Existential Deposit</a>
+      <span> requirements</span>
     </span>
   );
 };
 
 export const LowBalanceDescription: FC<Props> = ({ lowBalanceWallets }) => {
-  let isLowPolygonBalance = false;
+  let isLowPolkadotBalance = false;
   const walletNames = lowBalanceWallets.map((ecosystemId) => {
     if ([EcosystemId.Acala, EcosystemId.Karura].includes(ecosystemId)) {
-      isLowPolygonBalance = true;
+      isLowPolkadotBalance = true;
       return ecosystems[ecosystemId].displayName + "*";
     } else {
       return ecosystems[ecosystemId].displayName;
@@ -52,7 +52,7 @@ export const LowBalanceDescription: FC<Props> = ({ lowBalanceWallets }) => {
           <li key={name}>{name}</li>
         ))}
       </ul>
-      <LowPolygonBalanceWarning isVisible={isLowPolygonBalance} />
+      <LowPolkadotBalanceWarning isVisible={isLowPolkadotBalance} />
     </p>
   );
 };

4df336254d0c93e379268fa76743396510b2c360
Kirito <kirito@exsphere.com>
2022-06-22 12:26:45 -1000

    chore(ui): remove commentted code

--- apps/ui/src/models/swim/index.ts
+++ apps/ui/src/models/swim/index.ts
@@ -1,14 +1,11 @@
 export * from "./SwimDefiInstructor";
 export * from "./SwimInitializer";
-// export * from "./ampFactor";
-// export * from "./decimal";
 export * from "./fees";
 export * from "./instructions";
 export * from "./interaction";
 export * from "./operation";
 export * from "./pool";
 export * from "./poolMath";
-// export * from "./poolState";
 export * from "./steps";
 export * from "./stepsReducer";
 export * from "./stepsState";

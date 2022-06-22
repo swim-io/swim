6e1edf988290c9690e10f9a3449b39f0c7943082
Nico Miicro <nico@swim.io>
2022-06-22 13:19:02 +0300

    test(ui): update useWalletAdapter tests

--- apps/ui/src/core/store/tests/useWalletAdapter.test.ts
+++ apps/ui/src/core/store/tests/useWalletAdapter.test.ts
@@ -30,103 +30,191 @@ describe("useWalletAdapter", () => {
         protocol === Protocol.Evm
           ? WalletServiceId.MetaMask
           : WalletServiceId.Phantom;
-      it("connects to a service/protocol and stores the service id for the protocol", async () => {
-        const { result } = renderHook(() => useWalletAdapter());
 
-        const adapter = createWalletAdapter(protocol);
-        const connectSpy = jest
-          .spyOn(adapter, "connect")
-          .mockImplementation(
-            createMockConnectImplementation(adapter, protocol),
+      describe("connectService", () => {
+        it("connects to a service/protocol and stores the service id for the protocol", async () => {
+          const { result } = renderHook(() => useWalletAdapter());
+
+          const adapter = createWalletAdapter(protocol);
+          const connectSpy = jest
+            .spyOn(adapter, "connect")
+            .mockImplementation(
+              createMockConnectImplementation(adapter, protocol),
+            );
+
+          await act(() =>
+            result.current.connectService({ protocol, serviceId, adapter }),
           );
 
-        await act(() =>
-          result.current.connectService({ protocol, serviceId, adapter }),
-        );
-
-        expect(getProtocolAdapter(result.current, protocol)).toEqual(adapter);
-        expect(result.current.selectedServiceByProtocol[protocol]).toEqual(
-          serviceId,
-        );
-        expect(connectSpy).toHaveBeenCalledTimes(1);
-
-        expect(notificationStore.getState().toasts).toHaveLength(1);
-        expect(notificationStore.getState().toasts[0].title).toEqual(
-          "Wallet update",
-        );
-        expect(notificationStore.getState().toasts[0].text).toMatch(
-          "Connected to wallet",
-        );
-      });
-
-      it("connects to a service/protocol and disconnects the existing adapter", async () => {
-        const { result } = renderHook(() => useWalletAdapter());
-
-        const adapter = createWalletAdapter(protocol);
-        jest
-          .spyOn(adapter, "connect")
-          .mockImplementation(
-            createMockConnectImplementation(adapter, protocol),
-          );
-        const disconnectSpy = jest
-          .spyOn(adapter, "disconnect")
-          .mockImplementation(() => Promise.resolve());
-
-        await act(() =>
-          result.current.connectService({ protocol, serviceId, adapter }),
-        );
-
-        const secondAdapter = createWalletAdapter(protocol);
-        jest
-          .spyOn(adapter, "connect")
-          .mockImplementation(() => Promise.resolve());
-
-        await act(() =>
-          result.current.connectService({
-            protocol,
+          expect(getProtocolAdapter(result.current, protocol)).toEqual(adapter);
+          expect(result.current.selectedServiceByProtocol[protocol]).toEqual(
             serviceId,
-            adapter: secondAdapter,
-          }),
-        );
+          );
+          expect(connectSpy).toHaveBeenCalledTimes(1);
 
-        expect(getProtocolAdapter(result.current, protocol)).toEqual(
-          secondAdapter,
-        );
-        expect(disconnectSpy).toHaveBeenCalledTimes(1);
+          expect(notificationStore.getState().toasts).toHaveLength(1);
+          expect(notificationStore.getState().toasts[0].title).toEqual(
+            "Wallet update",
+          );
+          expect(notificationStore.getState().toasts[0].text).toMatch(
+            "Connected to wallet",
+          );
+        });
+
+        it("connects to a service/protocol and disconnects the existing adapter", async () => {
+          const { result } = renderHook(() => useWalletAdapter());
+
+          const adapter = createWalletAdapter(protocol);
+          jest
+            .spyOn(adapter, "connect")
+            .mockImplementation(
+              createMockConnectImplementation(adapter, protocol),
+            );
+          const disconnectSpy = jest
+            .spyOn(adapter, "disconnect")
+            .mockImplementation(() => Promise.resolve());
+
+          await act(() =>
+            result.current.connectService({ protocol, serviceId, adapter }),
+          );
+
+          const secondAdapter = createWalletAdapter(protocol);
+          jest
+            .spyOn(adapter, "connect")
+            .mockImplementation(() => Promise.resolve());
+
+          await act(() =>
+            result.current.connectService({
+              protocol,
+              serviceId,
+              adapter: secondAdapter,
+            }),
+          );
+
+          expect(getProtocolAdapter(result.current, protocol)).toEqual(
+            secondAdapter,
+          );
+          expect(disconnectSpy).toHaveBeenCalledTimes(1);
+        });
+
+        it("connects to a service/protocol and passes connectArgs to adapter's connect call", async () => {
+          const { result } = renderHook(() => useWalletAdapter());
+
+          const adapter = createWalletAdapter(protocol);
+          const connectSpy = jest
+            .spyOn(adapter, "connect")
+            .mockImplementation(
+              createMockConnectImplementation(adapter, protocol),
+            );
+
+          const connectArgs = { onlyIfTrusted: true as const };
+
+          await act(() =>
+            result.current.connectService({
+              protocol,
+              serviceId,
+              adapter,
+              options: { connectArgs },
+            }),
+          );
+
+          expect(connectSpy).toHaveBeenCalledTimes(1);
+          expect(connectSpy).toHaveBeenCalledWith(connectArgs);
+        });
+
+        it("connects to a service/protocol and silences error during connect", async () => {
+          const { result } = renderHook(() => useWalletAdapter());
+
+          const adapter = createWalletAdapter(protocol);
+          const connectSpy = jest
+            .spyOn(adapter, "connect")
+            .mockImplementation(() => {
+              adapter.emit("error", "error title", "error message");
+              return Promise.resolve();
+            });
+
+          await act(() =>
+            result.current.connectService({
+              protocol,
+              serviceId,
+              adapter,
+              options: { silentError: true },
+            }),
+          );
+
+          expect(connectSpy).toHaveBeenCalledTimes(1);
+          expect(notificationStore.getState().toasts).toHaveLength(0);
+        });
       });
 
-      it("disconnects the protocol's adapter", async () => {
-        const { result } = renderHook(() => useWalletAdapter());
+      describe("disconnectService", () => {
+        it("disconnects the protocol's adapter", async () => {
+          const { result } = renderHook(() => useWalletAdapter());
 
-        const adapter = createWalletAdapter(protocol);
-        jest
-          .spyOn(adapter, "connect")
-          .mockImplementation(
-            createMockConnectImplementation(adapter, protocol),
+          const adapter = createWalletAdapter(protocol);
+          jest
+            .spyOn(adapter, "connect")
+            .mockImplementation(
+              createMockConnectImplementation(adapter, protocol),
+            );
+
+          const disconnectSpy = jest
+            .spyOn(adapter, "disconnect")
+            .mockImplementation(
+              createMockDisconnectImplementation(adapter, protocol),
+            );
+
+          await act(() =>
+            result.current.connectService({ protocol, serviceId, adapter }),
           );
 
-        const disconnectSpy = jest
-          .spyOn(adapter, "disconnect")
-          .mockImplementation(
-            createMockDisconnectImplementation(adapter, protocol),
+          await act(() => result.current.disconnectService({ protocol }));
+
+          expect(getProtocolAdapter(result.current, protocol)).toBeNull();
+          expect(result.current.selectedServiceByProtocol[protocol]).toBeNull();
+          expect(disconnectSpy).toHaveBeenCalledTimes(1);
+
+          expect(notificationStore.getState().toasts).toHaveLength(2);
+          expect(notificationStore.getState().toasts[1].title).toEqual(
+            "Wallet update",
+          );
+          expect(notificationStore.getState().toasts[1].text).toEqual(
+            "Disconnected from wallet",
+          );
+        });
+
+        it("disconnects the protocol's adapter silently", async () => {
+          const { result } = renderHook(() => useWalletAdapter());
+
+          const adapter = createWalletAdapter(protocol);
+          jest
+            .spyOn(adapter, "connect")
+            .mockImplementation(
+              createMockConnectImplementation(adapter, protocol),
+            );
+
+          const disconnectSpy = jest
+            .spyOn(adapter, "disconnect")
+            .mockImplementation(
+              createMockDisconnectImplementation(adapter, protocol),
+            );
+
+          await act(() =>
+            result.current.connectService({ protocol, serviceId, adapter }),
           );
 
-        await act(() =>
-          result.current.connectService({ protocol, serviceId, adapter }),
-        );
+          await act(() =>
+            result.current.disconnectService({
+              protocol,
+              options: { silently: true },
+            }),
+          );
 
-        await act(() => result.current.disconnectService({ protocol }));
-
-        expect(getProtocolAdapter(result.current, protocol)).toBeNull();
-        expect(disconnectSpy).toHaveBeenCalledTimes(1);
-
-        expect(notificationStore.getState().toasts).toHaveLength(2);
-        expect(notificationStore.getState().toasts[1].title).toEqual(
-          "Wallet update",
-        );
-        expect(notificationStore.getState().toasts[1].text).toEqual(
-          "Disconnected from wallet",
-        );
+          expect(getProtocolAdapter(result.current, protocol)).toBeNull();
+          expect(result.current.selectedServiceByProtocol[protocol]).toBeNull();
+          expect(disconnectSpy).toHaveBeenCalledTimes(1);
+          expect(notificationStore.getState().toasts).toHaveLength(1);
+        });
       });
     });
   });

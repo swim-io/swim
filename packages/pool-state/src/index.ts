5c150eedf4e64eff53f057a60ad4ee8af25d6b4f
wormat <wormat@maelstrom.finance>
2022-06-23 12:42:27 -0400

    fix(pool-state): Fix exports

--- packages/pool-state/src/index.ts
+++ packages/pool-state/src/index.ts
@@ -1,5 +1,2 @@
-import type { SwimPoolState, } from "./poolState";
-import type { swimPool, deserializeSwimPool } from "./poolState";
-
-export type { SwimPoolState };
-export { swimPool, deserializeSwimPool };
+export type { SwimPoolState } from "./poolState";
+export { swimPool, deserializeSwimPool } from "./poolState";

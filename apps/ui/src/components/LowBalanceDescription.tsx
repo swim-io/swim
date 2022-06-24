6e472b825ac3b9cd156088f9649d4235bbdfe1c8
swimdrew <95261047+swimdrew@users.noreply.github.com>
2022-06-24 13:33:38 -0400

    Revert "Revert "Handle Existential Deposit errors and add warnings""

--- apps/ui/src/components/LowBalanceDescription.tsx
+++ apps/ui/src/components/LowBalanceDescription.tsx
@@ -1,16 +1,46 @@
-import type { FC } from "react";
+import type { FC, ReactElement } from "react";
 
-import type { EcosystemId } from "../config";
-import { ecosystems } from "../config";
+import { EcosystemId, ecosystems } from "../config";
+
+const EXISTENTIAL_DEPOSIT_LINK =
+  "https://support.polkadot.network/support/solutions/articles/65000168651-what-is-the-existential-deposit";
 
 interface Props {
   readonly lowBalanceWallets: readonly EcosystemId[];
 }
 
-export const LowBalanceDescription: FC<Props> = ({ lowBalanceWallets }) => {
-  const walletNames = lowBalanceWallets.map(
-    (ecosystemId) => ecosystems[ecosystemId].displayName,
+interface LowPolkadotBalanceWarningProps {
+  readonly isVisible: boolean;
+}
+
+const LowPolkadotBalanceWarning = ({
+  isVisible,
+}: LowPolkadotBalanceWarningProps): ReactElement | null => {
+  if (!isVisible) {
+    return null;
+  }
+  return (
+    <span>
+      <span>
+        *Polkadot chains require a minimum balance in order not to be
+        deactivated according to
+      </span>
+      <a href={EXISTENTIAL_DEPOSIT_LINK}> Existential Deposit</a>
+      <span> requirements</span>
+    </span>
   );
+};
+
+export const LowBalanceDescription: FC<Props> = ({ lowBalanceWallets }) => {
+  let isLowPolkadotBalance = false;
+  const walletNames = lowBalanceWallets.map((ecosystemId) => {
+    if ([EcosystemId.Acala, EcosystemId.Karura].includes(ecosystemId)) {
+      isLowPolkadotBalance = true;
+      return ecosystems[ecosystemId].displayName + "*";
+    } else {
+      return ecosystems[ecosystemId].displayName;
+    }
+  });
   return (
     <p>
       <span>
@@ -22,6 +52,7 @@ export const LowBalanceDescription: FC<Props> = ({ lowBalanceWallets }) => {
           <li key={name}>{name}</li>
         ))}
       </ul>
+      <LowPolkadotBalanceWarning isVisible={isLowPolkadotBalance} />
     </p>
   );
 };

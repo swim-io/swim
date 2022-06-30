54791ebeca71161fac8dfc095a21abde61763ad8
wormat <wormat@maelstrom.finance>
2022-06-30 19:09:24 +0200

    refactor: Improve naming in plugin-ecosystem-solana

--- packages/plugin-ecosystem-solana/src/index.ts
+++ packages/plugin-ecosystem-solana/src/index.ts
@@ -1,4 +1,4 @@
-import type { ChainSpec, EcosystemSpec } from "./base";
+import type { ChainConfig, EcosystemConfig } from "./base";
 import { Env } from "./base";
 
 /** Adapted from @solana/spl-token-registry ENV */
@@ -9,7 +9,7 @@ export const enum SolanaChainId {
   Localnet = 104,
 }
 
-export interface SolanaChainSpec extends ChainSpec {
+export interface SolanaChainConfig extends ChainConfig {
   /** This should be unique for a given Env */
   readonly chainId: SolanaChainId;
   readonly endpoint: string;
@@ -18,20 +18,18 @@ export interface SolanaChainSpec extends ChainSpec {
   readonly otterTotCollection: string;
 }
 
-export interface SolanaEcosystemSpec extends EcosystemSpec {
-  readonly chainSpecs: readonly SolanaChainSpec[];
+export interface SolanaEcosystemConfig extends EcosystemConfig {
+  readonly chains: readonly SolanaChainConfig[];
 }
 
-export const PRESETS: ReadonlyMap<Env, SolanaChainSpec> = new Map([
+export const PRESETS: ReadonlyMap<Env, SolanaChainConfig> = new Map([
   [
     Env.Mainnet,
     {
       env: Env.Mainnet,
       chainId: SolanaChainId.MainnetBeta,
-      wormhole: {
-        bridge: "worm2ZoG2kUd4vFXhvjh93UUH596ayRfgQ2MgjNMTth",
-        tokenBridge: "wormDTUJ6AWPNvk59vGQbDvGJmqbDTdgWgAqcLBCgUb",
-      },
+      wormholeBridge: "worm2ZoG2kUd4vFXhvjh93UUH596ayRfgQ2MgjNMTth",
+      wormholeTokenBridge: "wormDTUJ6AWPNvk59vGQbDvGJmqbDTdgWgAqcLBCgUb",
       endpoint: "https://solana-api.projectserum.com",
       wsEndpoint: "",
       tokenContract: "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
@@ -43,10 +41,8 @@ export const PRESETS: ReadonlyMap<Env, SolanaChainSpec> = new Map([
     {
       env: Env.Devnet,
       chainId: SolanaChainId.Devnet,
-      wormhole: {
-        bridge: "3u8hJUVTA4jH1wYAyUur7FFZVQ8H635K3tSHHF4ssjQ5",
-        tokenBridge: "DZnkkTmCiFWfYTfT41X3Rd1kDgozqzxWaHqsw6W4x2oe",
-      },
+      wormholeBridge: "3u8hJUVTA4jH1wYAyUur7FFZVQ8H635K3tSHHF4ssjQ5",
+      wormholeTokenBridge: "DZnkkTmCiFWfYTfT41X3Rd1kDgozqzxWaHqsw6W4x2oe",
       endpoint: "https://api.devnet.solana.com",
       wsEndpoint: "",
       tokenContract: "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
@@ -56,14 +52,14 @@ export const PRESETS: ReadonlyMap<Env, SolanaChainSpec> = new Map([
 ]);
 
 const createEcosystemConfig = (
-  chainSpecs: readonly SolanaChainSpec[],
-): EcosystemSpec => ({
+  chains: readonly SolanaChainConfig[],
+): SolanaEcosystemConfig => ({
   id: "solana" as const,
   protocol: "solana-protocol" as const,
   wormholeChainId: 1 as const,
   displayName: "Solana",
   nativeTokenSymbol: "SOL",
-  chainSpecs,
+  chains,
 });
 
 export default createEcosystemConfig;

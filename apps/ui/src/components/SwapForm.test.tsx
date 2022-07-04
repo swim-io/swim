7cbd18d051d9df735a5fe2649db0f0ed0ebd1f76
Nico Miicro <nico@swim.io>
2022-07-04 11:05:38 +0300

    test(ui): add tests for SwapFrom
    and remove unnecessary mocking

--- apps/ui/src/components/SwapForm.test.tsx
+++ apps/ui/src/components/SwapForm.test.tsx
@@ -1,66 +1,33 @@
-import { act, screen } from "@testing-library/react";
+import { act, fireEvent, screen, waitFor } from "@testing-library/react";
+import type { FC } from "react";
 
-import { EcosystemId, Env } from "../config";
+import { Env } from "../config";
 import { useEnvironment as environmentStore } from "../core/store";
-import {
-  useGetSwapFormErrors,
-  useSwapFeesEstimationQuery,
-  useSwapOutputAmountEstimate,
-  useUserBalanceAmounts,
-  useUserNativeBalances,
-} from "../hooks";
-import { mockOf, renderWithAppContext } from "../testUtils";
+import { renderWithAppContext } from "../testUtils";
 
 import { SwapForm } from "./SwapForm";
 
-jest.mock("../hooks", () => ({
-  ...jest.requireActual("../hooks"),
-  useUserNativeBalances: jest.fn(),
-  useUserBalanceAmounts: jest.fn(),
-  useSwapOutputAmountEstimate: jest.fn(),
-  useSwapFeesEstimationQuery: jest.fn(),
-  useGetSwapFormErrors: jest.fn(),
-}));
-
-jest.mock("../hooks/interaction", () => ({
-  ...jest.requireActual("../hooks/interaction"),
-  useHasActiveInteraction: jest.fn(),
-  useStartNewInteraction: jest.fn(),
-}));
-
-// Make typescript happy with jest
-const useUserNativeBalancesMock = mockOf(useUserNativeBalances);
-const useUserBalanceAmountsMock = mockOf(useUserBalanceAmounts);
-const useSwapOutputAmountEstimateMock = mockOf(useSwapOutputAmountEstimate);
-const useSwapFeesEstimationQueryMock = mockOf(useSwapFeesEstimationQuery);
-const useGetSwapFormErrorsMock = mockOf(useGetSwapFormErrors);
+// EuiSelectable uses EuiSelectableList which uses
+// react-virtualized-auto-sizer and needs some dimensions mocking in
+// order to render anything. See https://github.com/bvaughn/react-virtualized/issues/493
+jest.mock(
+  "react-virtualized-auto-sizer",
+  (): FC<any> =>
+    ({ children }) =>
+      children({ height: 600, width: 600 }),
+);
 
 describe("SwapForm", () => {
   beforeEach(() => {
-    useUserBalanceAmountsMock.mockReturnValue({
-      [EcosystemId.Acala]: null,
-      [EcosystemId.Aurora]: null,
-      [EcosystemId.Avalanche]: null,
-      [EcosystemId.Bnb]: null,
-      [EcosystemId.Ethereum]: null,
-      [EcosystemId.Fantom]: null,
-      [EcosystemId.Karura]: null,
-      [EcosystemId.Polygon]: null,
-      [EcosystemId.Solana]: null,
-    });
-    useUserNativeBalancesMock.mockReturnValue({});
-    useSwapOutputAmountEstimateMock.mockReturnValue(null);
-    useSwapFeesEstimationQueryMock.mockReturnValue(null);
-    useGetSwapFormErrorsMock.mockReturnValue(() => []);
-
     // currently we can't change the env unless a custom localnet ip is set
     environmentStore.getState().setCustomLocalnetIp("122.122.122.12");
+    environmentStore.getState().setEnv(Env.Mainnet);
+
+    renderWithAppContext(<SwapForm maxSlippageFraction={null} />);
   });
 
   it("should update token options when env changes", () => {
     const { env, setEnv } = environmentStore.getState();
-
-    renderWithAppContext(<SwapForm maxSlippageFraction={null} />);
 
     expect(env).toBe(Env.Mainnet);
     expect(screen.queryAllByRole("button")[0]).toHaveTextContent(
@@ -72,6 +39,52 @@ describe("SwapForm", () => {
     expect(environmentStore.getState().env).toBe(Env.Devnet);
     expect(screen.queryAllByRole("button")[0]).toHaveTextContent(
       "USDC on Ethereum",
+    );
+  });
+
+  it("should update toToken options when fromToken changes", async () => {
+    // assert initial value of toToken
+    expect(screen.queryAllByRole("button")[3]).toHaveTextContent(
+      "USDT on Solana",
+    );
+
+    // click toToken button
+    fireEvent.click(screen.queryAllByRole("button")[0]);
+
+    // wait for TokenSearchModal to render
+    await waitFor(() => {
+      return screen.findByPlaceholderText("Search tokens");
+    });
+
+    // Click on GST option
+    fireEvent.click(screen.getByTitle("GST Green Satoshi Token BNB Chain"));
+
+    // assert toToken has updated
+    expect(screen.queryAllByRole("button")[3]).toHaveTextContent(
+      "GST on Solana",
+    );
+  });
+
+  it("should update toToken options when fromToken is updated with toToken value", async () => {
+    // assert initial value of toToken
+    expect(screen.queryAllByRole("button")[3]).toHaveTextContent(
+      "USDT on Solana",
+    );
+
+    // click toToken button
+    fireEvent.click(screen.queryAllByRole("button")[0]);
+
+    // wait for TokenSearchModal to render
+    await waitFor(() => {
+      return screen.findByPlaceholderText("Search tokens");
+    });
+
+    // Click on GST option
+    fireEvent.click(screen.getByTitle("USDT Tether USD Solana"));
+
+    // assert toToken has updated
+    expect(screen.queryAllByRole("button")[3]).toHaveTextContent(
+      "USDC on Solana",
     );
   });
 });

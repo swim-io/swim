a0efc484d51aac3003de46e0921ec7635c9ea86f
Kirito <kirito@exsphere.com>
2022-07-05 03:49:31 -1000

    chore: rename bsc to bnb chain

--- packages/plugin-ecosystem-bnb/src/index.ts
+++ packages/plugin-ecosystem-bnb/src/index.ts
@@ -1,17 +1,17 @@
 import type { EvmChainConfig, EvmEcosystemConfig } from '@swim-io/evm-config';
 import { Env } from "@swim-io/core-types";
 
-const BscEcosystemId = "bsc";
-const BscMainnetChainId = 56;
-const BscTestnetChainId = 97;
-const BscWormholeChainId = 4;
+const BnbEcosystemId = "bsc";
+const BnbMainnetChainId = 56;
+const BnbTestnetChainId = 97;
+const BnbWormholeChainId = 4;
 
 export const PRESETS: ReadonlyMap<Env, EvmEcosystemConfig> = new Map([
   [
     Env.Mainnet,
     {
-      ecosystem: BscEcosystemId,
-      chainId: BscMainnetChainId,
+      ecosystem: BnbEcosystemId,
+      chainId: BnbMainnetChainId,
       chainName: "BNB Chain Mainnet",
       rpcUrls: ["https://bsc-dataseed1.ninicoin.io"], // TODO: Think about what is best to recommend to MetaMask
       wormholeBridge: "0x98f3c9e6E3fAce36bAAd05FE09d375Ef1464288B",
@@ -21,8 +21,8 @@ export const PRESETS: ReadonlyMap<Env, EvmEcosystemConfig> = new Map([
   [
     Env.Devnet,
     {
-      ecosystem: BscEcosystemId,
-      chainId: BscTestnetChainId,
+      ecosystem: BnbEcosystemId,
+      chainId: BnbTestnetChainId,
       chainName: "BNB Chain Testnet",
       rpcUrls: ["https://data-seed-prebsc-2-s2.binance.org:8545"],
       wormholeBridge: "0x68605AD7b15c732a30b1BbC62BE8F2A509D74b4D",
@@ -31,13 +31,13 @@ export const PRESETS: ReadonlyMap<Env, EvmEcosystemConfig> = new Map([
   ],
 ]);
 
-export const createBsdEcosystemConfig = (
+export const createBnbEcosystemConfig = (
   chains: readonly EvmChainConfig[],
 ): EvmEcosystemConfig => ({
-  id: BscEcosystemId,
+  id: BnbEcosystemId,
   protocol: "evm" as const,
-  wormholeChainId: BscWormholeChainId,
-  displayName: "Ethereum",
+  wormholeChainId: BnbWormholeChainId,
+  displayName: "BNB",
   gasToken: {
     name: "Binance Coin",
     symbol: "BNB",

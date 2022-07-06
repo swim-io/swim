6a0c913e5e4f81b826abe3cf93e3d017a7681dac
wormat <wormat@maelstrom.finance>
2022-07-06 13:27:39 +0200

    refactor: Make core-types package more generic

--- packages/core-types/src/core.ts
+++ packages/core-types/src/core.ts
@@ -5,24 +5,31 @@ export enum Env {
   Custom = "Custom",
 }
 
-export interface ChainConfig {
+export interface ChainConfig<E extends string, C extends number> {
+  readonly name: string;
   readonly env: Env;
-  readonly chainId: number;
+  readonly ecosystemId: E;
+  readonly chainId: C;
   readonly wormholeBridge: string;
   readonly wormholeTokenBridge: string;
 }
 
-interface GasToken {
+export interface GasToken {
   readonly name: string;
   readonly symbol: string;
   readonly decimals: number;
 }
 
-export interface EcosystemConfig {
-  readonly id: string;
-  readonly protocol: string;
-  readonly wormholeChainId: number;
+export interface EcosystemConfig<
+  P extends string,
+  E extends string,
+  W extends number,
+  C extends number,
+> {
+  readonly id: E;
+  readonly protocol: P;
+  readonly wormholeChainId: W;
   readonly displayName: string;
   readonly gasToken: GasToken;
-  readonly chains: readonly ChainConfig[];
+  readonly chains: readonly ChainConfig<E, C>[];
 }

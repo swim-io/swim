d88272c7856e25566d6393fc0dbdacb431050abf
swimivan <ivan@swim.io>
2022-07-06 15:56:27 -0700

    refactor: Clean up and fix EVM contracts further

--- packages/evm-contracts/contracts/ERC20.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract SwimUSD is ERC20 {
-    constructor() ERC20("SwimUSD", "sUSD") {}
-}
--- packages/evm-contracts/contracts/interfaces/ISwimUSD.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
-
-interface ISwimUSD is IERC20Upgradeable {}

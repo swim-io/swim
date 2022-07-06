de85e5f688c0be056a7dfbdc8af8e3f2d004e1a4
Kirito <kirito@exsphere.com>
2022-07-06 03:55:48 -1000

    chore: update package.json and change comment to docstring

--- packages/evm-types/src/index.ts
+++ packages/evm-types/src/index.ts
@@ -2,7 +2,8 @@ import type { ChainConfig, EcosystemConfig } from "@swim-io/core-types";
 
 export interface EvmChainConfig extends ChainConfig {
   readonly ecosystem: string,
-  readonly chainId: number; // This should be unique for a given Env
+  /** This should be unique for a given Env */
+  readonly chainId: number;
   readonly chainName: string;
   readonly rpcUrls: readonly string[];
 }

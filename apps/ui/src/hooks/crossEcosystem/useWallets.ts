bc2497d14753f4c4b0c5d9d9c74e853a48fa2402
swimdrew <andrew@kvasium.com>
2022-07-05 17:52:30 -0700

    refactor: move wallet utils to its own file

--- apps/ui/src/hooks/crossEcosystem/useWallets.ts
+++ apps/ui/src/hooks/crossEcosystem/useWallets.ts
@@ -1,22 +1,8 @@
-import { EcosystemId, isEcosystemEnabled } from "../../config";
-import type { BaseWallet } from "../../models";
-import type { ReadonlyRecord } from "../../utils";
-import type { EvmWalletInterface } from "../evm";
+import { EcosystemId } from "../../config";
 import { useEvmWallet } from "../evm/useEvmWallet";
-import type { SolanaWalletInterface } from "../solana";
 import { useSolanaWallet } from "../solana/useSolanaWallet";
 
-export interface Wallets extends ReadonlyRecord<EcosystemId, BaseWallet> {
-  readonly [EcosystemId.Solana]: SolanaWalletInterface;
-  readonly [EcosystemId.Ethereum]: EvmWalletInterface;
-  readonly [EcosystemId.Bnb]: EvmWalletInterface;
-  readonly [EcosystemId.Avalanche]: EvmWalletInterface;
-  readonly [EcosystemId.Polygon]: EvmWalletInterface;
-  readonly [EcosystemId.Aurora]: EvmWalletInterface;
-  readonly [EcosystemId.Fantom]: EvmWalletInterface;
-  readonly [EcosystemId.Karura]: EvmWalletInterface;
-  readonly [EcosystemId.Acala]: EvmWalletInterface;
-}
+import { Wallets } from "../utils"
 
 export const useWallets = (): Wallets => ({
   [EcosystemId.Solana]: useSolanaWallet(),
@@ -29,38 +15,3 @@ export const useWallets = (): Wallets => ({
   [EcosystemId.Karura]: useEvmWallet(),
   [EcosystemId.Acala]: useEvmWallet(),
 });
-
-export const getAddressesByEcosystem = (
-  wallets: Wallets,
-): ReadonlyRecord<EcosystemId, string | null> =>
-  Object.entries(wallets).reduce<ReadonlyRecord<string, string | null>>(
-    (accumulator, [ecosystemId, { address }]) => ({
-      ...accumulator,
-      [ecosystemId]: address,
-    }),
-    {},
-  );
-
-export const getAddressesForEcosystems = (
-  ecosystems: readonly EcosystemId[],
-  wallets: Wallets,
-): ReadonlyRecord<EcosystemId, string | null> =>
-  ecosystems.reduce<ReadonlyRecord<string, string | null>>(
-    (accumulator, ecosystemId) => ({
-      ...accumulator,
-      [ecosystemId]: wallets[ecosystemId].address,
-    }),
-    {},
-  );
-
-export const isEveryAddressConnected = (
-  addresses: ReadonlyRecord<EcosystemId, string | null>,
-  wallets: Wallets,
-): boolean => {
-  return Object.entries(addresses).every(
-    ([ecosystemId, address]) =>
-      address === null ||
-      (isEcosystemEnabled(ecosystemId as EcosystemId) &&
-        address === wallets[ecosystemId as EcosystemId].address),
-  );
-};
--- apps/ui/src/hooks/utils/index.ts
+++ apps/ui/src/hooks/utils/index.ts
@@ -1,1 +1,2 @@
 export * from "./useAsyncGenerator";
+export * from "./walletUtils";

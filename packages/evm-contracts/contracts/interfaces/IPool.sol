30b1b71e25fd6788e2dedde4f8762020946d7591
cypher <cypher@exsphere.com>
2022-07-06 14:41:16 +0200

    chore: remove types

--- packages/evm-contracts/contracts/interfaces/IPool.sol
+++ packages/evm-contracts/contracts/interfaces/IPool.sol
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-/*
- * @title Pool contract
- *
- */
-
 interface IPool {
   function swap(
     uint256 inputAmount,
@@ -13,6 +8,37 @@ interface IPool {
     uint8 outputTokenIndex,
     uint256 minimumOutputAmount
   ) external returns (uint256 outputAmount);
+
+  function swapExactOutput(
+    uint maximumInputAmount,
+    uint8 inputTokenIndex,
+    uint[] memory outputAmounts
+  ) external returns(uint inputAmount);
+
+  function swapExactInput(
+    uint[] memory inputAmounts,
+    uint8 outputTokenIndex,
+    uint minimumOutputAmount
+  ) external returns(uint outputAmount);
+
+  function removeExactBurn(
+    uint burnAmount,
+    uint8 outputTokenIndex,
+    uint minimumOutputAmount
+  ) external returns(uint outputAmount);
+
+  function removeExactOutput(
+    uint[] memory outputAmounts,
+    uint maximumBurnAmount
+  ) external returns(uint burnAmount);
+
+  function add(
+    uint[] memory inputAmounts,
+    uint minimumMintAmount
+  ) external returns(uint mintAmount);
+
+  function removeUniform(uint burnAmount, uint[] memory minimumOutputAmounts)
+    external returns(uint[] memory outputAmounts);
 
   function getTokenCount() external returns (uint8 tokenCount);
 

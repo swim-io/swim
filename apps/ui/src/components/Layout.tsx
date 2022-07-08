15d93c52a5ab1bccda7162ecbc822920a4787adc
Nico Miicro <nico@swim.io>
2022-07-08 16:51:08 +0300

    fix(ui): fix second scrollbar

--- apps/ui/src/components/Layout.tsx
+++ apps/ui/src/components/Layout.tsx
@@ -5,6 +5,7 @@ import {
   EuiSpacer,
 } from "@elastic/eui";
 import type { ReactElement, ReactNode } from "react";
+import { useLocation } from "react-router-dom";
 
 import { MultiConnectButton } from "./ConnectButton";
 import { CustomHeaderLink } from "./CustomHeaderLink";
@@ -20,41 +21,40 @@ export const Layout = ({
 }: {
   readonly children?: ReactNode;
 }): ReactElement => {
+  const footer = useLocation().pathname === "/swap" ? null : <Footer />;
+
   return (
-    <>
-      <Waves>
-        <EuiSpacer />
-        <EuiSpacer />
-        <EuiHeader
-          position="fixed"
-          sections={[
-            {
-              items: [
-                <CustomHeaderLogo key="custom-header-logo" />,
-                <EuiHeaderLinks key="eui-header-links">
-                  <CustomHeaderLink to={"/swap"}>Swap</CustomHeaderLink>
-                  <CustomHeaderLink to={"/pools"}>Pools</CustomHeaderLink>
-                  {/* TODO: Enable when token is launched */}
-                  {/* <CustomHeaderLink to={"/stake"}>Stake</CustomHeaderLink> */}
-                  <CustomHeaderLink to={"/help"}>Help</CustomHeaderLink>
-                </EuiHeaderLinks>,
-              ],
-              borders: "right",
-            },
-            {
-              items: [
-                <EuiHeaderSectionItem key="eui-header-section-item">
-                  <MultiConnectButton size="s" fullWidth />
-                </EuiHeaderSectionItem>,
-                <EnvSelector key="env-selector" />,
-              ],
-              borders: "none",
-            },
-          ]}
-        />
-        {children}
-      </Waves>
-      <Footer />
-    </>
+    <Waves footer={footer}>
+      <EuiSpacer />
+      <EuiSpacer />
+      <EuiHeader
+        position="fixed"
+        sections={[
+          {
+            items: [
+              <CustomHeaderLogo key="custom-header-logo" />,
+              <EuiHeaderLinks key="eui-header-links">
+                <CustomHeaderLink to={"/swap"}>Swap</CustomHeaderLink>
+                <CustomHeaderLink to={"/pools"}>Pools</CustomHeaderLink>
+                {/* TODO: Enable when token is launched */}
+                {/* <CustomHeaderLink to={"/stake"}>Stake</CustomHeaderLink> */}
+                <CustomHeaderLink to={"/help"}>Help</CustomHeaderLink>
+              </EuiHeaderLinks>,
+            ],
+            borders: "right",
+          },
+          {
+            items: [
+              <EuiHeaderSectionItem key="eui-header-section-item">
+                <MultiConnectButton size="s" fullWidth />
+              </EuiHeaderSectionItem>,
+              <EnvSelector key="env-selector" />,
+            ],
+            borders: "none",
+          },
+        ]}
+      />
+      {children}
+    </Waves>
   );
 };

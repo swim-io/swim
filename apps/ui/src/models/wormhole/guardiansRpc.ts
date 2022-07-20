cf6310fda77bbdc2ae84a26446a5e33edc0280aa
Nico Miicro <nico@swim.io>
2022-07-20 15:48:30 +0300

    feat(ui): handle more wormhole error cases

--- apps/ui/src/models/wormhole/guardiansRpc.ts
+++ apps/ui/src/models/wormhole/guardiansRpc.ts
@@ -3,6 +3,7 @@ import type { RpcError } from "grpc-web";
 import { StatusCode } from "grpc-web";
 
 import { SwimError } from "../../errors";
+import type { ReadonlyRecord } from "../../utils";
 
 const isRpcError = (error: unknown): error is RpcError => {
   return (
@@ -13,18 +14,30 @@ const isRpcError = (error: unknown): error is RpcError => {
 };
 
 // Common RPC errors as seen in Swim
-// message: requested VAA not found in store, code: 5
-// message: Response closed without headers, code: 2
+// message: requested VAA not found in store, code: 5 (StatusCode.NOT_FOUND)
+// message: Response closed without headers, code: 2 (StatusCode.UNKNOWN)
+
+const INTERNAL_ERROR_MESSAGE =
+  "Something went wrong, please contact Swim support.";
+const UNAVAILABLE_MESSAGE =
+  "We are unable to reach the Wormhole guardians. Please try again later.";
+
+const MESSAGES: ReadonlyRecord<StatusCode, string | undefined> = {
+  [StatusCode.INTERNAL]: INTERNAL_ERROR_MESSAGE,
+  [StatusCode.INVALID_ARGUMENT]: INTERNAL_ERROR_MESSAGE,
+  [StatusCode.NOT_FOUND]:
+    "Could not confirm transfer with Wormhole guardians. This usually happens when the source blockchain is congested. Please try again later.",
+  [StatusCode.UNAVAILABLE]: UNAVAILABLE_MESSAGE,
+  [StatusCode.UNKNOWN]: UNAVAILABLE_MESSAGE,
+};
 
 export const getSignedVaaWithRetry: typeof originalGetSignedVAAWithRetry = (
   ...args
 ) =>
   originalGetSignedVAAWithRetry(...args).catch((error) => {
-    if (isRpcError(error) && error.code === StatusCode.UNKNOWN) {
-      throw new SwimError(
-        "We are unable to reach the wormhole network. Please try again later.",
-        error,
-      );
+    if (isRpcError(error)) {
+      const message = MESSAGES[error.code];
+      if (message) throw new SwimError(message, error);
     }
 
     throw error;

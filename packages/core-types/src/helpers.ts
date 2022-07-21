632b5d46fec5c13dc0bb4424545ce58e97ae4bae
wormat <wormat@maelstrom.finance>
2022-07-21 18:41:37 +0100

    refactor: Remove helpers from core-types

--- packages/core-types/src/helpers.ts
+++
@@ -1,3 +0,0 @@
-export type ReadonlyRecord<K extends string | number | symbol, T> = Readonly<
-  Record<K, T>
->;
--- packages/core-types/src/index.ts
+++ packages/core-types/src/index.ts
@@ -1,7 +1,6 @@
 export * from "./chain";
 export * from "./ecosystem";
 export * from "./env";
-export * from "./helpers";
 export * from "./pool";
 export * from "./token";
 export * from "./tx";

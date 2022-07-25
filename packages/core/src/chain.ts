4f4c1cd94703ed54fdfdb451eeda6b8514b0a09e
wormat <wormat@maelstrom.finance>
2022-07-25 19:12:10 +0200

    refactor(core): Adjust wormhole properties in ChainConfig

--- packages/core/src/chain.ts
+++ packages/core/src/chain.ts
@@ -1,11 +1,17 @@
 import type { PoolConfig } from "./pool";
 import type { TokenConfig } from "./token";
 
+export interface WormholeConfig {
+  /** The core (generic) VAA bridge contract */
+  readonly bridge: string;
+  /** The portal token bridge application contract */
+  readonly portal: string;
+}
+
 export interface ChainConfig {
   readonly name: string;
   readonly chainId: number;
-  readonly wormholeBridge: string;
-  readonly wormholeTokenBridge: string;
+  readonly wormhole: WormholeConfig;
   readonly tokens: readonly TokenConfig[];
   readonly pools: readonly PoolConfig[];
 }

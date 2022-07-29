e5bc60adfa2643281d84ac49f5fcb88fae5886d4
swimdrew <andrew@kvasium.com>
2022-07-29 11:38:16 -0400

    fix: merge

--- apps/ui/src/components/SwapForm/SwapForm.tsx
+++ apps/ui/src/components/SwapForm/SwapForm.tsx
@@ -24,7 +24,7 @@ import {
   useSplTokenAccountsQuery,
   useSwapFeesEstimationQuery,
   useSwapOutputAmountEstimate,
-  useSwapTokens,
+  useSwapTokensContext,
   useUserBalanceAmounts,
   useUserNativeBalances,
 } from "../../hooks";
@@ -64,11 +64,13 @@ export const SwapForm = ({ maxSlippageFraction }: Props): ReactElement => {
   const {
     fromToken,
     toToken,
-    setFromTokenId,
-    setToTokenId,
     fromTokenOptionsIds,
     toTokenOptionsIds,
-  } = useSwapTokens();
+    setFromToken,
+    setToToken,
+    setFromAndToTokens,
+    hasUrlError,
+  } = useSwapTokensContext();
   const [formErrors, setFormErrors] = useState<readonly string[]>([]);
 
   const requiredPools = getRequiredPoolsForSwap(
@@ -231,6 +233,7 @@ export const SwapForm = ({ maxSlippageFraction }: Props): ReactElement => {
   const isStableSwap = requiredPools.every((pool) => pool.isStableSwap);
   return (
     <EuiForm component="form" className="swapForm" onSubmit={handleSubmit}>
+      {hasUrlError && <EuiCallOut title="Invalid URL inputs" color="danger" />}
       <EuiSpacer />
 
       <TokenAmountInput
@@ -240,7 +243,7 @@ export const SwapForm = ({ maxSlippageFraction }: Props): ReactElement => {
         placeholder={"Enter amount"}
         disabled={isInteractionInProgress}
         errors={inputAmountErrors}
-        onSelectToken={setFromTokenId}
+        onSelectToken={setFromToken}
         onChangeValue={(value) => setFormInputAmount(value)}
         onBlur={() => handleInputAmountChange(inputAmount)}
         showConstantSwapTip={!isStableSwap}
@@ -253,8 +256,7 @@ export const SwapForm = ({ maxSlippageFraction }: Props): ReactElement => {
           size="m"
           iconSize="xl"
           onClick={() => {
-            setFromTokenId(toToken.id);
-            setToTokenId(fromToken.id);
+            setFromAndToTokens(toToken, fromToken);
           }}
           className="swapForm__flipIcon"
           aria-label="Flip direction"
@@ -273,7 +275,7 @@ export const SwapForm = ({ maxSlippageFraction }: Props): ReactElement => {
         placeholder={"Output"}
         disabled={isInteractionInProgress}
         errors={[]}
-        onSelectToken={setToTokenId}
+        onSelectToken={setToToken}
         // Never show constant swap on "To Form".
         showConstantSwapTip={false}
       />
--- apps/ui/src/pages/SwapPage/SwapPage.tsx
+++ apps/ui/src/pages/SwapPage/SwapPage.tsx
@@ -18,9 +18,10 @@ import shallow from "zustand/shallow.js";
 import { RecentInteractions } from "../../components/RecentInteractions";
 import { SlippageButton } from "../../components/SlippageButton";
 import { SwapForm } from "../../components/SwapForm";
+import { ECOSYSTEMS } from "../../config";
 import { selectConfig } from "../../core/selectors";
 import { useEnvironment } from "../../core/store";
-import { useTitle } from "../../hooks";
+import { useSwapTokensContext, useTitle } from "../../hooks";
 import { INTERACTION_GROUP_SWAP } from "../../models";
 
 import "./SwapPage.scss";
@@ -28,7 +29,12 @@ import "./SwapPage.scss";
 const SwapPage = (): ReactElement => {
   const { pools } = useEnvironment(selectConfig, shallow);
 
-  useTitle("Swap");
+  const { fromToken, toToken } = useSwapTokensContext();
+  const fromEcosystemName = ECOSYSTEMS[fromToken.nativeEcosystem].displayName;
+  const toEcosystemName = ECOSYSTEMS[toToken.nativeEcosystem].displayName;
+  useTitle(
+    `Swap ${fromEcosystemName} ${fromToken.project.displayName} to ${toEcosystemName} ${toToken.project.displayName}`,
+  );
 
   const nonStakingPools = useMemo(
     () => pools.filter((pool) => !pool.isStakingPool),

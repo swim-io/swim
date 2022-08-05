3b0ba59c52e00a74daecc02f540654d966ba9115
Thor <109517311+swimthor@users.noreply.github.com>
2022-08-05 22:04:18 +0100

    chore(ui): use eslint to ensure translation keys are sorted

--- apps/ui/.eslintrc.cjs
+++ apps/ui/.eslintrc.cjs
@@ -1,67 +1,87 @@
 require("@swim-io/eslint-config/patch/modern-module-resolution.cjs");
 
 module.exports = {
-  plugins: ["i18next"],
-  extends: ["@swim-io/eslint-config/react", "plugin:i18next/recommended"],
-  parserOptions: {
-    // Make sure correct `tsconfig.json` is found in monorepo
-    tsconfigRootDir: __dirname,
-  },
-  rules: {
-    // Enable periodically - we don’t want the linter to force us to upgrade at inconvenient times
-    "deprecation/deprecation": "off",
-    // no point exporting unused code since this isn't a library
-    "import/no-unused-modules": ["error", { unusedExports: true }],
-    // allow mutable state on class instances and zustand drafts
-    "functional/immutable-data": [
-      "error",
-      { ignorePattern: ["this", "draft"] },
-    ],
-    // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-render-in-setup.md
-    "testing-library/no-render-in-setup": [
-      "error",
-      { allowTestingFrameworkSetupHook: "beforeEach" },
-    ],
-  },
   overrides: [
     {
-      files: ["**/*.d.ts"],
-      rules: {
-        "import/unambiguous": "off",
-      },
+      files: ["**/*.{cjs,js,mts}"],
+      extends: ["@swim-io/eslint-config"],
     },
     {
-      files: [
-        "src/**/*.stories.@(js|jsx|ts|tsx)",
-        "src/**/*.test.@(js|jsx|ts|tsx)",
+      files: ["**/*.{cts,mts,ts,tsx}"],
+      plugins: ["i18next"],
+      extends: ["@swim-io/eslint-config/react", "plugin:i18next/recommended"],
+      parserOptions: {
+        // Make sure correct `tsconfig.json` is found in monorepo
+        tsconfigRootDir: __dirname,
+      },
+      rules: {
+        // Enable periodically - we don’t want the linter to force us to upgrade at inconvenient times
+        "deprecation/deprecation": "off",
+        // no point exporting unused code since this isn't a library
+        "import/no-unused-modules": ["error", { unusedExports: true }],
+        // allow mutable state on class instances and zustand drafts
+        "functional/immutable-data": [
+          "error",
+          { ignorePattern: ["this", "draft"] },
+        ],
+        // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-render-in-setup.md
+        "testing-library/no-render-in-setup": [
+          "error",
+          { allowTestingFrameworkSetupHook: "beforeEach" },
+        ],
+      },
+      overrides: [
+        {
+          files: ["**/*.d.ts"],
+          rules: {
+            "import/unambiguous": "off",
+          },
+        },
+        {
+          files: ["src/**/*.stories.{ts,tsx}", "src/**/*.test.{ts,tsx}"],
+          rules: {
+            // used when asserting with expect.stringMatching() where it returns `any`
+            "@typescript-eslint/no-unsafe-assignment": "off",
+            // allow mutation in test files for convenience
+            "functional/immutable-data": "off",
+            // these are expected by Storybook
+            "import/no-anonymous-default-export": "off",
+          },
+        },
+        {
+          files: [
+            "src/**/*.stories.{ts,tsx}",
+            "src/__mocks__/**/*",
+            "src/fixtures/**/*",
+          ],
+          rules: {
+            // Storybook/Jest magic
+            // keep fixtures around even if they aren't used right now
+            "import/no-unused-modules": "off",
+          },
+        },
+        {
+          files: ["src/contracts/**/*.ts"],
+          rules: {
+            // eslint comments are auto generated
+            "eslint-comments/no-unlimited-disable": "off",
+          },
+        },
       ],
-      rules: {
-        // used when asserting with expect.stringMatching() where it returns `any`
-        "@typescript-eslint/no-unsafe-assignment": "off",
-        // allow mutation in test files for convenience
-        "functional/immutable-data": "off",
-        // these are expected by Storybook
-        "import/no-anonymous-default-export": "off",
-      },
     },
     {
-      files: [
-        "src/**/*.stories.@(js|jsx|ts|tsx)",
-        "src/__mocks__/**/*",
-        "src/fixtures/**/*",
+      files: ["**/*.json"],
+      parser: "jsonc-eslint-parser",
+      overrides: [
+        {
+          files: ["src/locales/**/*.json"],
+          plugins: ["eslint-plugin-jsonc"],
+          extends: ["plugin:jsonc/recommended-with-jsonc"],
+          rules: {
+            "jsonc/sort-keys": "error",
+          },
+        },
       ],
-      rules: {
-        // Storybook/Jest magic
-        // keep fixtures around even if they aren't used right now
-        "import/no-unused-modules": "off",
-      },
-    },
-    {
-      files: ["src/contracts/**/*.ts"],
-      rules: {
-        // eslint comments are auto generated
-        "eslint-comments/no-unlimited-disable": "off",
-      },
     },
   ],
 };

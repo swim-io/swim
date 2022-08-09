c419ccb7ca3e5574075b8192f5f601f5447a92af
cypher <cypher@exsphere.com>
2022-08-09 19:10:24 +0200

    fix: change indexed fields in onChainSwap

--- packages/evm-contracts/contracts/interfaces/IRouting.sol
+++ packages/evm-contracts/contracts/interfaces/IRouting.sol
@@ -10,9 +10,9 @@ interface IRouting {
   event TokenRegistered(uint16 indexed tokenId, address indexed tokenContract, address pool);
 
   event OnChainSwap(
-    address to,
-    address indexed fromToken,
-    address indexed toToken,
+    address indexed to,
+    address fromToken,
+    address toToken,
     uint256 outputAmount,
     bytes16 indexed interactionId
   );

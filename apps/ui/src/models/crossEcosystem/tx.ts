fb9533003d43a2e06bc690788326539cce2c8012
wormat <wormat@maelstrom.finance>
2022-08-15 12:50:49 +0200

    WIP: refactor(ui): Use ecosystem ID types/values from solana/evm packages

--- apps/ui/src/models/crossEcosystem/tx.ts
+++ apps/ui/src/models/crossEcosystem/tx.ts
@@ -1,29 +1,4 @@
-import type { EvmTx as BaseEvmTx } from "@swim-io/evm";
-import type { SolanaTx as BaseSolanaTx } from "@swim-io/solana";
-import type { ReadonlyRecord } from "@swim-io/utils";
-
-import type { EvmEcosystemId } from "../../config";
-import { EcosystemId, isEvmEcosystemId } from "../../config";
-
-export interface SolanaTx extends BaseSolanaTx {
-  readonly ecosystemId: EcosystemId.Solana;
-}
-
-export interface EvmTx extends BaseEvmTx {
-  readonly ecosystemId: EvmEcosystemId;
-}
+import type { EvmTx } from "@swim-io/evm";
+import type { SolanaTx } from "@swim-io/solana";
 
 export type Tx = SolanaTx | EvmTx;
-
-export type TxsByTokenId = ReadonlyRecord<string, readonly Tx[] | undefined>;
-
-export interface TxWithTokenId<T extends Tx = Tx> {
-  readonly tokenId: string;
-  readonly tx: T;
-}
-
-export const isSolanaTx = (tx: Tx): tx is SolanaTx =>
-  tx.ecosystemId === EcosystemId.Solana;
-
-export const isEvmTx = (tx: Tx): tx is EvmTx =>
-  isEvmEcosystemId(tx.ecosystemId);

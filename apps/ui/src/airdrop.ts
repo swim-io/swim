0d68cf01bea3bd79641a13d02b06d6882a05f003
Fis <fis@swim.io>
2022-08-18 02:27:03 +0300

    feat(utils): WIP Solana devnet tokens airdrop
    It worked and then stopped. Doesn't build because [react-ace (used by
    elastic UI) can't doesn't support server-side rendering][1]
    
    [1]: https://github.com/securingsincity/react-ace/issues/27#issuecomment-597902860

--- apps/ui/src/airdrop.ts
+++ apps/ui/src/airdrop.ts
@@ -1,56 +1,49 @@
-import yargs, { string } from "yargs"
-import { hideBin } from "yargs/helpers"
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+import { ethers } from "ethers";
+import {
+  Connection,
+  PublicKey,
+  ConfirmOptions,
+  clusterApiUrl,
+  Keypair,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+import { Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import Decimal from "decimal.js";
+import * as bip39 from "bip39";
+import { derivePath } from "ed25519-hd-key";
 
 import { Env } from "@swim-io/core";
 import { findOrThrow } from "@swim-io/utils";
-import { ethers } from "ethers";
 
-import type { Config, EvmSpec, TokenSpec } from "./config";
+import type { Config, EvmSpec, SolanaSpec, TokenSpec } from "./config";
 import { CONFIGS, Protocol } from "./config";
-import { Erc20Factory, EvmConnection } from "./models/evm";
+import { Erc20Factory, EvmConnection, Amount } from "./models";
 
-import "dotenv/config"
+import "dotenv/config";
 
-async function main() {
-  // Get tokens and chains configs
-  const ENV = Env.Devnet;
-  const { tokens, chains }: Config = CONFIGS[ENV];
+type CliOptions = Awaited<ReturnType<typeof parseCliOptions>>;
 
-  const args = await yargs(hideBin(process.argv))
-    .scriptName("airdrop")
-    .usage("$0 [args]")
-    .option("token", {
-      type: "string",
-      choices: tokens.map(({ id }: TokenSpec) => id),
-      demandOption: true,
-    })
-    .option("receiverAddress", {
-      type: "string",
-      alias: "to",
-      demandOption: true,
-    })
-    .option("amount", {
-      type: "string",
-    })
-    .help()
-    .parse()
-
+async function transferOnEvm(args: CliOptions) {
   const tokenId = args.token;
   const receiverAddress = args.receiverAddress;
-  const amount = args.amount || 1;
+  const amount = args.amount;
 
   const tokenSpec = findOrThrow(tokens, ({ id }: TokenSpec) => id === tokenId);
 
   // Get a token contract
   const chainSpec = findOrThrow(
     chains[Protocol.Evm] as EvmSpec[],
-    ({ ecosystem }: EvmSpec) => ecosystem === tokenSpec.nativeEcosystemId,
-    );
+    ({ ecosystem }) => ecosystem === tokenSpec.nativeEcosystemId,
+  );
 
-    const evmProvider = EvmConnection.getIndexerProvider(ENV, chainSpec);
+  const evmProvider = EvmConnection.getIndexerProvider(ENV, chainSpec);
 
   // Hippo wallet (dev bank)
-  const mnemonic = process.env.DEVNET_MNEMONIC_PHRASE;
+  const mnemonic =
+    "track hybrid update chat play mimic warrior ozone liquid empower knee resist";
   const senderWallet = ethers.Wallet.fromMnemonic(mnemonic.trim()).connect(
     evmProvider,
   );
@@ -99,6 +92,169 @@ async function main() {
       await evmProvider.getBalance(senderWallet.address),
     )}`,
   );
+}
+
+async function transferOnSolana(args: CliOptions) {
+  const receiverAddress = new PublicKey(args.receiverAddress);
+
+  const tokenId = args.token;
+  const tokenSpec = findOrThrow(tokens, ({ id }: TokenSpec) => id === tokenId);
+
+  const amount = Amount.fromHumanString(tokenSpec, args.amount);
+
+  // Get a token contract
+  const chainSpec = findOrThrow(
+    chains[Protocol.Solana] as SolanaSpec[],
+    ({ ecosystem }) => ecosystem === tokenSpec.nativeEcosystemId,
+  );
+
+  const rpcUrl = clusterApiUrl("devnet");
+
+  const confirmOptions: ConfirmOptions = {
+    commitment: "confirmed",
+    preflightCommitment: "confirmed",
+  };
+
+  // Initialize a Solana Connection
+  const solanaConnection = new Connection(rpcUrl, confirmOptions);
+
+  // Gather required keys
+  // Hippo dev
+  const mnemonic =
+    "under pond submit fix spoon chef always picture ugly farm helmet stable loud morning drastic swap appear wrong define clump donkey expand buyer ski";
+  const seed = await bip39.mnemonicToSeed(mnemonic, "");
+  const path = `m/44'/501'/0'/0'`;
+  const fromWallet = Keypair.fromSeed(
+    derivePath(path, seed.toString("hex")).key,
+  );
+
+  console.log(
+    `User wallet public key: ${path} => ${fromWallet.publicKey.toBase58()}`,
+  );
+
+  // Get a token contract address
+  const tokenContractAddress = new PublicKey(tokenSpec.nativeDetails.address);
+
+  console.log(
+    `Attempting to send ${amount.toString()} ${tokenSpec.projectId} on ${
+      tokenSpec.nativeEcosystemId
+    } (${tokenContractAddress}) from ${
+      fromWallet.publicKey
+    } to ${receiverAddress}`,
+  );
+
+  const fromToken = new Token(
+    solanaConnection,
+    tokenContractAddress,
+    TOKEN_PROGRAM_ID,
+    fromWallet,
+  );
+  const fromTokenAccount = await fromToken.getOrCreateAssociatedAccountInfo(
+    fromWallet.publicKey,
+  );
+
+  const toTokenAccount = await fromToken.getOrCreateAssociatedAccountInfo(
+    receiverAddress,
+  );
+
+  // Check account balance
+  const senderTokenBalance = await solanaConnection.getTokenAccountBalance(
+    fromTokenAccount.address,
+  );
+
+  const balance = await solanaConnection.getBalance(fromWallet.publicKey);
+  // Convert lamports to SOL
+  const senderSolBalance = new Decimal(balance).dividedBy(LAMPORTS_PER_SOL);
+
+  console.log(
+    `Sender ${fromWallet.publicKey} has ${senderTokenBalance.value.amount} ${tokenSpec.projectId} on ${tokenSpec.nativeEcosystemId}`,
+  );
+  console.log(
+    `Sender (${fromWallet.publicKey}) has ${senderTokenBalance.value.amount} ${tokenSpec.projectId}. Gas tokens balance: ${senderSolBalance}`,
+  );
+
+  const txSignature = await fromToken.transfer(
+    fromTokenAccount.address,
+    toTokenAccount.address,
+    fromWallet,
+    [fromWallet],
+    amount.toAtomic(tokenSpec.nativeEcosystemId).toNumber(),
+  );
+
+  console.log(`Transaction signature: ${txSignature}.`);
+
+  console.log(
+    `Balance after. Sender: ${await solanaConnection.getTokenAccountBalance(
+      fromWallet.publicKey,
+    )} ${
+      tokenSpec.projectId
+    }. Receiver: ${await solanaConnection.getTokenAccountBalance(
+      receiverAddress,
+    )} ${tokenSpec.projectId}. Gas tokens: ${await solanaConnection.getBalance(
+      fromWallet.publicKey,
+    )}`,
+  );
+}
+
+// Get tokens and chains configs
+const ENV = Env.Devnet;
+const { tokens, chains }: Config = CONFIGS[ENV];
+
+async function parseCliOptions() {
+  return yargs(hideBin(process.argv))
+    .scriptName("airdrop")
+    .usage("$0 [args]")
+    .option("token", {
+      type: "string",
+      choices: tokens.map(({ id }: TokenSpec) => id),
+      demandOption: true,
+    })
+    .option("receiverAddress", {
+      type: "string",
+      alias: "to",
+      demandOption: true,
+    })
+    .option("amount", {
+      type: "string",
+      default: "1",
+    })
+    .check((argv, options) => {
+      const { token: tokenId, receiverAddress } = argv;
+      const tokenSpec = tokens.find(({ id }: TokenSpec) => id === tokenId);
+
+      if (!tokenSpec) {
+        throw new Error(`Token ${tokenId} not found.`);
+      }
+
+      if (
+        ethers.utils.isAddress(receiverAddress) ||
+        PublicKey.isOnCurve(new PublicKey(receiverAddress))
+      ) {
+        return true;
+      } else {
+        throw new Error(`Receiver address ${receiverAddress} does not exist.`);
+      }
+    })
+    .help()
+    .parse();
+}
+
+async function main() {
+  const args = await parseCliOptions();
+
+  const receiverAddress = args.receiverAddress;
+
+  if (
+    !args.token.startsWith("devnet-solana-") &&
+    ethers.utils.isAddress(receiverAddress)
+  ) {
+    await transferOnEvm(args);
+  } else if (
+    args.token.startsWith("devnet-solana-") &&
+    PublicKey.isOnCurve(new PublicKey(receiverAddress))
+  ) {
+    await transferOnSolana(args);
+  }
 }
 
 main()

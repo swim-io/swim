d2da9a8d8b35c1d5752c87e5f86362f49b7c5b6e
swimricky <ricky@swim.io>
2022-09-06 10:39:42 -0400

    docs: add comment for deps scope in commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,6 +7,7 @@ const dirPaths = fg.sync(packageJson.workspaces, {
   onlyFiles: false,
   markDirectories: true,
 });
+// manually adding "deps" as scope for dependabot PRs
 const scopes = [
   ...dirPaths.map((dirPath) => path.basename(dirPath)),
   "deps",

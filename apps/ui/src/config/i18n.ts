5a3300b3b7e8bc4c0cb88620f795f951554b5807
Thor <109517311+swimthor@users.noreply.github.com>
2022-09-06 18:37:24 +0100

    feat(ui): enable ja/ru/zh-CN translations

--- apps/ui/src/config/i18n.ts
+++ apps/ui/src/config/i18n.ts
@@ -2,8 +2,14 @@ import type { ReadonlyRecord } from "@swim-io/utils";
 
 export enum Language {
   En = "en",
+  Ja = "ja",
+  Ru = "ru",
+  ZhCn = "zh-CN",
 }
 
 export const languageNameMapping: ReadonlyRecord<Language, string> = {
   [Language.En]: "English",
+  [Language.Ja]: "日本語",
+  [Language.Ru]: "Русский язык",
+  [Language.ZhCn]: "简体中文",
 };

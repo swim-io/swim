67c5fdb717f78d3478b9db987e2fff6a342c383e
Thor <109517311+swimthor@users.noreply.github.com>
2022-09-12 16:47:04 +0100

    refactor(ui): move getSignedVaaWithRetry to wormhole package

--- apps/ui/src/models/wormhole/guardiansRpc.ts
+++ apps/ui/src/models/wormhole/guardiansRpc.ts
@@ -1,45 +1,35 @@
-import { getSignedVAAWithRetry as originalGetSignedVAAWithRetry } from "@certusone/wormhole-sdk";
 import type { ReadonlyRecord } from "@swim-io/utils";
-import type { RpcError } from "grpc-web";
-import { StatusCode } from "grpc-web";
+import {
+  WormholeError,
+  WormholeErrorCode,
+  getSignedVaaWithRetry as originalGetSignedVAAWithRetry,
+} from "@swim-io/wormhole";
 
 import { SwimError } from "../../errors";
 import { i18next } from "../../i18n";
 
-const isRpcError = (error: unknown): error is RpcError => {
-  return (
-    error instanceof Error &&
-    "code" in error &&
-    Object.values(StatusCode).includes(
-      (error as Record<string, unknown>).code as string,
-    )
-  );
-};
-
 export const getSignedVaaWithRetry: typeof originalGetSignedVAAWithRetry =
   async (...args) => {
-    const INTERNAL_ERROR_MESSAGE = i18next.t("general.internal_vaa_error");
-    const UNAVAILABLE_MESSAGE = i18next.t("general.unreachable_vaa_error");
-
-    // Common RPC errors as seen in Swim
-    // message: requested VAA not found in store, code: 5 (StatusCode.NOT_FOUND)
-    // message: Response closed without headers, code: 2 (StatusCode.UNKNOWN)
-    const MESSAGES: Partial<ReadonlyRecord<StatusCode, string>> = {
-      [StatusCode.INTERNAL]: INTERNAL_ERROR_MESSAGE,
-      [StatusCode.INVALID_ARGUMENT]: INTERNAL_ERROR_MESSAGE,
-      [StatusCode.NOT_FOUND]: i18next.t(
+    const messageMapping: ReadonlyRecord<WormholeErrorCode, string> = {
+      [WormholeErrorCode.GuardiansCannotConfirmTransfer]: i18next.t(
         "general.cannot_confirm_transfer_vaa_error",
       ),
-      [StatusCode.UNAVAILABLE]: UNAVAILABLE_MESSAGE,
-      [StatusCode.UNKNOWN]: UNAVAILABLE_MESSAGE,
+      [WormholeErrorCode.GuardiansCannotReach]: i18next.t(
+        "general.unreachable_vaa_error",
+      ),
+      [WormholeErrorCode.InternalError]: i18next.t(
+        "general.internal_vaa_error",
+      ),
     };
 
-    return await originalGetSignedVAAWithRetry(...args).catch((error) => {
-      if (isRpcError(error)) {
-        const message = MESSAGES[error.code];
-        if (message) throw new SwimError(message, error);
-      }
+    return await originalGetSignedVAAWithRetry(...args).catch(
+      (error: unknown) => {
+        if (error instanceof WormholeError) {
+          const message = messageMapping[error.code];
+          if (message) throw new SwimError(message, error.originalError);
+        }
 
-      throw error;
-    });
+        throw error;
+      },
+    );
   };

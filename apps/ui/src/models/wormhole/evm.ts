d17f6fee363e6e1d23f522b266c1e2ce3f423801
Chiu <chiu@exsphere.com>
2022-09-13 18:34:29 +0800

    chore(ui): Single chain evm swap

--- apps/ui/src/models/wormhole/evm.ts
+++ apps/ui/src/models/wormhole/evm.ts
@@ -1,12 +1,14 @@
 import { getAllowanceEth } from "@certusone/wormhole-sdk";
 import { PublicKey } from "@solana/web3.js";
-import type { WormholeChainConfig } from "@swim-io/core";
-import type { EvmTx } from "@swim-io/evm";
+import type { TokenDetails, WormholeChainConfig } from "@swim-io/core";
+import type { EvmTx, EvmWalletAdapter } from "@swim-io/evm";
 import { approveEth, transferFromEth } from "@swim-io/wormhole";
 import type { ethers } from "ethers";
 
-import type { TokenConfig } from "../../config";
+import type { EvmSpec, TokenConfig } from "../../config";
 import { WormholeChainId, getTokenDetailsForEcosystem } from "../../config";
+import type { Amount } from "../amount";
+import type { EvmConnection } from "../evm";
 
 import type { WormholeTransfer } from "./transfer";
 
@@ -48,12 +50,70 @@ export const isUnlockEvmTx = (
   );
 };
 
+export const approveAmount = async (
+  amount: Amount,
+  evmChain: EvmSpec,
+  evmConnection: EvmConnection,
+  fromTokenDetails: TokenDetails,
+  evmWallet: EvmWalletAdapter,
+  spenderAddress: string,
+): Promise<readonly ethers.providers.TransactionResponse[]> => {
+  const evmSigner = evmWallet.signer;
+  if (!evmSigner) {
+    throw new Error("No EVM signer");
+  }
+
+  await evmWallet.switchNetwork(evmChain.chainId);
+
+  const transferAmountAtomicString = amount.toAtomicString(evmChain.ecosystem);
+  const allowance = await getAllowanceEth(
+    spenderAddress,
+    fromTokenDetails.address,
+    evmSigner,
+  );
+
+  let approvalResponses: readonly ethers.providers.TransactionResponse[] = [];
+  if (allowance.lt(transferAmountAtomicString)) {
+    if (!allowance.isZero()) {
+      // Reset to 0 to avoid a race condition allowing Wormhole to steal funds
+      // See https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+      // Note this is required by some ERC20 implementations such as USDT
+      // See line 205 here: https://etherscan.io/address/0xdac17f958d2ee523a2206206994597c13d831ec7#code
+      const resetApprovalResponse = await approveEth(
+        spenderAddress,
+        fromTokenDetails.address,
+        evmSigner,
+        "0",
+      );
+      approvalResponses = resetApprovalResponse
+        ? [...approvalResponses, resetApprovalResponse]
+        : approvalResponses;
+    }
+    const approvalResponse = await approveEth(
+      spenderAddress,
+      fromTokenDetails.address,
+      evmSigner,
+      transferAmountAtomicString,
+    );
+    approvalResponses = approvalResponse
+      ? [...approvalResponses, approvalResponse]
+      : approvalResponses;
+
+    // Wait for approvalResponse to be mined, otherwise the transfer might fail ("transfer amount exceeds allowance")
+    if (approvalResponse) {
+      await evmConnection.getTxReceiptOrThrow(approvalResponse);
+    }
+  }
+
+  return approvalResponses;
+};
+
 export const lockEvmToken = async ({
   interactionId,
   amount,
   evmChain,
   evmConnection,
-  fromTokenDetails: evmTokenDetails,
+  fromTokenDetails,
   evmWallet,
   splTokenAccountAddress,
 }: WormholeTransfer): Promise<{
@@ -68,50 +128,20 @@ export const lockEvmToken = async ({
   await evmWallet.switchNetwork(evmChain.chainId);
 
   const transferAmountAtomicString = amount.toAtomicString(evmChain.ecosystem);
-  const allowance = await getAllowanceEth(
+  const approvalResponses = await approveAmount(
+    amount,
+    evmChain,
+    evmConnection,
+    fromTokenDetails,
+    evmWallet,
     evmChain.wormhole.portal,
-    evmTokenDetails.address,
-    evmSigner,
   );
-
-  let approvalResponses: readonly ethers.providers.TransactionResponse[] = [];
-  if (allowance.lt(transferAmountAtomicString)) {
-    if (!allowance.isZero()) {
-      // Reset to 0 to avoid a race condition allowing Wormhole to steal funds
-      // See https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-      // Note this is required by some ERC20 implementations such as USDT
-      // See line 205 here: https://etherscan.io/address/0xdac17f958d2ee523a2206206994597c13d831ec7#code
-      const resetApprovalResponse = await approveEth(
-        evmChain.wormhole.portal,
-        evmTokenDetails.address,
-        evmSigner,
-        "0",
-      );
-      approvalResponses = resetApprovalResponse
-        ? [...approvalResponses, resetApprovalResponse]
-        : approvalResponses;
-    }
-    const approvalResponse = await approveEth(
-      evmChain.wormhole.portal,
-      evmTokenDetails.address,
-      evmSigner,
-      transferAmountAtomicString,
-    );
-    approvalResponses = approvalResponse
-      ? [...approvalResponses, approvalResponse]
-      : approvalResponses;
-
-    // Wait for approvalResponse to be mined, otherwise the transfer might fail ("transfer amount exceeds allowance")
-    if (approvalResponse) {
-      await evmConnection.getTxReceiptOrThrow(approvalResponse);
-    }
-  }
 
   const transferResponse = await transferFromEth(
     interactionId,
     evmChain.wormhole.portal,
     evmSigner,
-    evmTokenDetails.address,
+    fromTokenDetails.address,
     transferAmountAtomicString,
     WormholeChainId.Solana,
     new PublicKey(splTokenAccountAddress).toBytes(),

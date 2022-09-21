cb1795e8183987becf7fcb3adce23b36987ec3c3
cypher <103903636+cyphertrace@users.noreply.github.com>
2022-09-21 16:28:11 +0200

    [UI] separate ecosytem and token (#387)
    * fix: refacto swap
    
    * chore: redesign swap
    
    * chore: translations and cleanup
    
    * fix: lint issues
    
    * refactor: remove network title
    
    * refactor: replace panel with separation between network and tokens
    
    * fix: minor fixes
    
    * fix: minor fixes
    
    * fix: modal title change
    
    * refactor: review suggestions
    
    * refactor: move connect button next to slippage
    
    * fix: buttons alignement
    
    * chore: set from amount to 2 decimals
    
    * fix: input placeholder
    
    * fix: change placeholder
    
    * fix: types
    
    * fix: init value and wordings

--- apps/ui/src/components/TokenSearchModal.tsx
+++ apps/ui/src/components/TokenSearchModal.tsx
@@ -1,4 +1,8 @@
 import {
+  EuiButton,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiHorizontalRule,
   EuiModalBody,
   EuiModalHeader,
   EuiModalHeaderTitle,
@@ -6,52 +10,62 @@ import {
 } from "@elastic/eui";
 import type { EuiSelectableOption } from "@elastic/eui";
 import { TOKEN_PROJECTS_BY_ID } from "@swim-io/token-projects";
-import { findOrThrow } from "@swim-io/utils";
-import type { ReactElement } from "react";
 import { useCallback } from "react";
+import type { ReactElement } from "react";
 import { useTranslation } from "react-i18next";
 import shallow from "zustand/shallow.js";
 
-import type { TokenConfig } from "../config";
-import { ECOSYSTEMS } from "../config";
+import type { EcosystemId, TokenConfig } from "../config";
+import { ECOSYSTEM_LIST, isEcosystemEnabled } from "../config";
 import { selectConfig } from "../core/selectors";
 import { useEnvironment } from "../core/store";
+import { useUserBalanceAmount } from "../hooks";
 
 import { CustomModal } from "./CustomModal";
-import { TokenConfigIcon } from "./TokenIcon";
+import { TokenSearchConfigIcon } from "./TokenIcon";
+
+import "./TokenSearchModal.scss";
 
 type TokenOption = EuiSelectableOption<{
   readonly data: Readonly<TokenConfig>;
 }>;
 
 const renderTokenOption = (option: TokenOption) => {
-  return <TokenConfigIcon token={option.data} />;
+  return <TokenSearchConfigIcon token={option.data} />;
 };
 
 interface Props {
   readonly handleClose: () => void;
   readonly handleSelectToken: (token: TokenConfig) => void;
+  readonly handleSelectEcosystem: (ecosystemId: EcosystemId) => void;
   readonly tokenOptionIds: readonly string[];
+  readonly selectedEcosystemId: EcosystemId;
 }
+
+interface TokenProps {
+  readonly token: TokenConfig;
+}
+
+const TokenBalance = ({ token }: TokenProps): ReactElement => {
+  const balance = useUserBalanceAmount(token, token.nativeEcosystemId);
+  return (
+    <span>{balance?.toFormattedHumanString(token.nativeEcosystemId)}</span>
+  );
+};
+
+const enabledEcosystems = ECOSYSTEM_LIST.filter((ecosystem) =>
+  isEcosystemEnabled(ecosystem.id),
+);
 
 export const TokenSearchModal = ({
   handleClose,
   handleSelectToken,
+  handleSelectEcosystem,
+  selectedEcosystemId,
   tokenOptionIds,
 }: Props): ReactElement => {
   const { t } = useTranslation();
   const { tokens } = useEnvironment(selectConfig, shallow);
-  const options = tokenOptionIds.map((tokenId) => {
-    const tokenConfig = findOrThrow(tokens, ({ id }) => id === tokenId);
-    const ecosystem = ECOSYSTEMS[tokenConfig.nativeEcosystemId];
-    const tokenProject = TOKEN_PROJECTS_BY_ID[tokenConfig.projectId];
-    return {
-      label: `${tokenProject.symbol} on ${ecosystem.displayName}`,
-      searchableLabel: `${tokenProject.symbol} ${tokenProject.displayName} ${ecosystem.displayName}`,
-      showIcons: false,
-      data: tokenConfig,
-    };
-  });
 
   const onSelectToken = useCallback(
     (opts: readonly TokenOption[]) => {
@@ -61,8 +75,25 @@ export const TokenSearchModal = ({
         handleClose();
       }
     },
-    [handleSelectToken, handleClose],
+    [handleClose, handleSelectToken],
   );
+
+  const filteredTokens = tokens.filter(
+    (token) =>
+      tokenOptionIds.includes(token.id) &&
+      token.nativeEcosystemId === selectedEcosystemId,
+  );
+
+  const options = filteredTokens.map((token) => {
+    const tokenProject = TOKEN_PROJECTS_BY_ID[token.projectId];
+    return {
+      label: `${tokenProject.symbol}`,
+      searchableLabel: `${tokenProject.symbol} ${tokenProject.displayName}`,
+      showIcons: false,
+      data: token,
+      append: <TokenBalance token={token} />,
+    };
+  });
 
   return (
     <CustomModal
@@ -72,20 +103,36 @@ export const TokenSearchModal = ({
     >
       <EuiModalHeader>
         <EuiModalHeaderTitle>
-          <h1>{t("token_search_modal.title")}</h1>
+          {t("token_select_modal.title")}
         </EuiModalHeaderTitle>
       </EuiModalHeader>
+      <EuiFlexGroup
+        responsive={false}
+        wrap
+        gutterSize="xs"
+        className="networkPanel"
+      >
+        {enabledEcosystems.map((ecosystem) => (
+          <EuiFlexItem grow={false} key={ecosystem.id}>
+            <EuiButton
+              fill={ecosystem.id === selectedEcosystemId}
+              onClick={() => handleSelectEcosystem(ecosystem.id)}
+              iconType={ecosystem.logo}
+              size="s"
+              minWidth={125}
+            >
+              {ecosystem.displayName}
+            </EuiButton>
+          </EuiFlexItem>
+        ))}
+      </EuiFlexGroup>
 
-      <EuiModalBody>
+      <EuiHorizontalRule margin="s" />
+
+      <EuiModalBody className="modalBody">
         <EuiSelectable
           options={options}
           renderOption={renderTokenOption}
-          searchable
-          searchProps={{
-            id: "token-search",
-            placeholder: t("token_search_modal.search_tokens"),
-            isClearable: true,
-          }}
           onChange={onSelectToken}
           singleSelection
           listProps={{
@@ -95,12 +142,7 @@ export const TokenSearchModal = ({
             },
           }}
         >
-          {(list, search) => (
-            <>
-              {search}
-              {list}
-            </>
-          )}
+          {(list) => <>{list}</>}
         </EuiSelectable>
       </EuiModalBody>
     </CustomModal>

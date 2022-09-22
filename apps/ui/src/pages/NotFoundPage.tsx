7258a4544332cecacc217d77c349f252bbbc6f12
swimdrew <andrew@kvasium.com>
2022-09-22 12:22:25 -0400

    fix: add english strings to localization file

--- apps/ui/src/pages/NotFoundPage.tsx
+++ apps/ui/src/pages/NotFoundPage.tsx
@@ -10,26 +10,35 @@ import {
   EuiTitle,
 } from "@elastic/eui";
 import type { ReactElement } from "react";
+import { Trans, useTranslation } from "react-i18next";
 
 import { useTitle } from "../hooks";
 
 const NotFoundPage = (): ReactElement => {
-  useTitle("Not Found");
+  const { t } = useTranslation();
+  useTitle(t("not_found.title"));
   return (
     <EuiPage className="notFoundPage" restrictWidth={800}>
       <EuiPageBody>
         <EuiPageContent verticalPosition="center">
           <EuiPageContentBody>
             <EuiTitle>
-              <h2>{"Sorry, this page isn't available"}</h2>
+              <h2>{t("not_found.header")}</h2>
             </EuiTitle>
             <EuiSpacer />
             <EuiText>
-              The link you followed may be broken, or the page may have been
-              removed.
-              <br/>
-              <br/>
-              Go back to the <a href={"https://swim.io"}>Swim home page</a>.
+              <p>
+                {t("not_found.body1")}
+                <br />
+                <br />
+                <Trans
+                  i18nKey="not_found.body2"
+                  components={{
+                    // eslint-disable-next-line jsx-a11y/anchor-has-content
+                    a: <a href="https://swim.io/" />,
+                  }}
+                />
+              </p>
             </EuiText>
           </EuiPageContentBody>
         </EuiPageContent>

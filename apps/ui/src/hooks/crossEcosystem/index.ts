88e8d0394356c4369cd0d503f72a1af3a1dc4277
wormat <wormat@maelstrom.finance>
2022-09-27 18:14:34 +0200

    refactor(ui): Remove unused useTokensByEcosystem hook

--- apps/ui/src/hooks/crossEcosystem/index.ts
+++ apps/ui/src/hooks/crossEcosystem/index.ts
@@ -1,6 +1,5 @@
 export * from "./useCoinGeckoPriceQuery";
 export * from "./useMultipleUserBalances";
-export * from "./useTokensByEcosystem";
 export * from "./useUserBalances";
 export * from "./useUserNativeBalances";
 export * from "./useWallets";

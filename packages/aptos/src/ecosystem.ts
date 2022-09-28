51f09a1c7deab4b05eb61822d6c6c3ba13c84e01
Nico Miicro <nico@swim.io>
2022-09-28 15:17:26 +0300

    chore(aptos): update wormhole testnet config

--- packages/aptos/src/ecosystem.ts
+++ packages/aptos/src/ecosystem.ts
@@ -5,16 +5,21 @@ import { assertType } from "@swim-io/utils";
 import type { AptosChainConfig, AptosEcosystemConfig } from "./protocol";
 import { APTOS_ECOSYSTEM_ID, APTOS_PROTOCOL } from "./protocol";
 
+// Visible on the explorer https://explorer.aptoslabs.com/
 export enum AptosChainId {
-  Devnet = 31, // announced in https://discord.com/channels/945856774056083548/956692649430093904
+  Devnet = 31,
 }
 
 const testnet: AptosChainConfig = {
   name: "Aptos Devnet",
   chainId: AptosChainId.Devnet,
   wormhole: {
-    bridge: "TODO",
-    portal: "TODO",
+    // https://github.com/wormhole-foundation/wormhole/blob/dee93c4eb39f90f1a09b7c2c62eed9d5a9279f4f/aptos/token_bridge/Move_testnet.toml#L20
+    bridge:
+      "0xdd0a2618dc5564ccf38d0eca7877198fef51157fea74a6bc2e5e40b52c2a0a08",
+    // https://github.com/wormhole-foundation/wormhole/blob/dee93c4eb39f90f1a09b7c2c62eed9d5a9279f4f/aptos/wormhole/Move_testnet.toml#L17
+    portal:
+      "0x1b1752e26b65fc24971ee5ec9718d2ccdd36bf20486a10b2973ea6dedc6cd197",
   },
   publicRpcUrls: ["https://fullnode.devnet.aptoslabs.com/v1"],
   swimUsdDetails: {

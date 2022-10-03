3ce7403f1bad58b529ba5cc30869820f20369491
swimricky <ricky@swim.io>
2022-10-03 15:45:49 -0400

    feat: remove unused marginal_price_pool file

--- packages/solana-contracts/programs/propeller/src/instructions/mod.rs
+++ packages/solana-contracts/programs/propeller/src/instructions/mod.rs
@@ -1,6 +1,6 @@
 pub use {
-    create_owner_token_accounts::*, fee_tracker::*, initialize::*, marginal_price_pool::*, process_swim_payload::*,
-    target_chain_map::*, token_id_map::*, utils::*, wormhole::*,
+    create_owner_token_accounts::*, fee_tracker::*, initialize::*, process_swim_payload::*, target_chain_map::*,
+    token_id_map::*, utils::*, wormhole::*,
 };
 
 pub mod process_swim_payload;
@@ -9,7 +9,6 @@ pub mod initialize;
 // pub mod pool;
 pub mod create_owner_token_accounts;
 pub mod fee_tracker;
-pub mod marginal_price_pool;
 pub mod target_chain_map;
 pub mod token_id_map;
 pub mod two_pool_cpi;

b4bbae95a83998c75498b6c81772edbb5b4d9e2e
Nico Miicro <nico@swim.io>
2022-10-17 12:06:02 +0300

    chore(aptos): update testnet details

--- packages/aptos/src/ecosystem.ts
+++ packages/aptos/src/ecosystem.ts
@@ -5,25 +5,24 @@ import { assertType } from "@swim-io/utils";
 import type { AptosChainConfig, AptosEcosystemConfig } from "./protocol";
 import { APTOS_ECOSYSTEM_ID, APTOS_PROTOCOL } from "./protocol";
 
-// Visible on the explorer https://explorer.aptoslabs.com/
 export enum AptosChainId {
-  Devnet = 31,
+  Testnet = 31, // TODO verify this, not sure if we need it?
 }
 
 const testnet: AptosChainConfig = {
-  name: "Aptos Devnet",
-  chainId: AptosChainId.Devnet,
+  name: "Aptos Testnet",
+  chainId: AptosChainId.Testnet,
   wormhole: {
-    // https://github.com/wormhole-foundation/wormhole/blob/dee93c4eb39f90f1a09b7c2c62eed9d5a9279f4f/aptos/token_bridge/Move_testnet.toml#L20
+    // from https://book.wormhole.com/reference/contracts.html
     bridge:
       "0xdd0a2618dc5564ccf38d0eca7877198fef51157fea74a6bc2e5e40b52c2a0a08",
-    // https://github.com/wormhole-foundation/wormhole/blob/dee93c4eb39f90f1a09b7c2c62eed9d5a9279f4f/aptos/wormhole/Move_testnet.toml#L17
     portal:
       "0x1b1752e26b65fc24971ee5ec9718d2ccdd36bf20486a10b2973ea6dedc6cd197",
   },
-  publicRpcUrls: ["https://fullnode.devnet.aptoslabs.com/v1"],
+  publicRpcUrls: ["https://testnet.aptoslabs.com/v1"],
   swimUsdDetails: {
-    address: "", // TODO: add when deployed,
+    address:
+      "0xcadb45f34eff49d40875c4160880b6944de8261b955b42f6f084e771859f12a7::coin::T", // TODO: add when deployed,
     decimals: 8, // TODO: confirm when deployed
   },
   routingContractAddress: "", // TODO: add when deployed

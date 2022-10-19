e3de24d2a29c4f3a2c75e0dd3f12f49405e1dd32
Chiu <chiu@exsphere.com>
2022-10-20 00:46:26 +0800

    chore(ui): fix test

--- apps/ui/src/fixtures/solana/wallet.ts
+++ apps/ui/src/fixtures/solana/wallet.ts
@@ -3,5 +3,6 @@ import { PublicKey } from "@solana/web3.js";
 
 export const MOCK_SOL_WALLET = {
   publicKey: new PublicKey("6sbzC1eH4FTujJXWj51eQe25cYvr4xfXbJ1vAj7j2k5J"),
+  address: "6sbzC1eH4FTujJXWj51eQe25cYvr4xfXbJ1vAj7j2k5J",
   signTransaction: (transaction: Transaction) => Promise.resolve(transaction),
 };

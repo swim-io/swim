7d41cb2e4126b200e594c7c4bcecae173fee6330
cypher <cypher@exsphere.com>
2022-10-19 18:48:27 +0200

    feat: wormhole form

---
+++ apps/ui/src/components/WormholeForm/WormholeTokenModal.tsx
@@ -0,0 +1,92 @@
+import type { EuiSelectableOption } from "@elastic/eui";
+import {
+  EuiModalBody,
+  EuiModalHeader,
+  EuiModalHeaderTitle,
+  EuiSelectable,
+} from "@elastic/eui";
+import { useCallback } from "react";
+import type { ReactElement, ReactNode } from "react";
+import { useTranslation } from "react-i18next";
+
+import type { WormholeToken } from "../../models";
+import { CustomModal } from "../CustomModal";
+import { WormholeTokenIcon } from "../TokenIcon";
+
+import "./WormholeTokenModal.scss";
+
+type TokenOption = EuiSelectableOption<{
+  readonly data: Readonly<WormholeToken>;
+}>;
+
+const renderTokenOption = (option: TokenOption): ReactNode => (
+  <WormholeTokenIcon token={option.data} isSelected={false} />
+);
+
+interface Props {
+  readonly handleClose: () => void;
+  readonly handleSelectToken: (token: WormholeToken) => void;
+  readonly tokens: readonly WormholeToken[];
+}
+
+export const WormholeTokenModal = ({
+  handleClose,
+  handleSelectToken,
+  tokens,
+}: Props): ReactElement => {
+  const { t } = useTranslation();
+
+  const options = tokens.map((token) => ({
+    label: `${token.symbol} ${token.displayName}`,
+    searchableLabel: `${token.symbol} ${token.displayName}`,
+    showIcons: false,
+    data: token,
+  }));
+
+  const onSelectToken = useCallback(
+    (opts: readonly TokenOption[]) => {
+      const selected = opts.find((token: TokenOption) => token.checked);
+      if (selected) {
+        handleSelectToken(selected.data);
+        handleClose();
+      }
+    },
+    [handleClose, handleSelectToken],
+  );
+
+  return (
+    <CustomModal
+      initialFocus="#token-search"
+      onClose={handleClose}
+      className="wormholeModal"
+    >
+      <EuiModalHeader>
+        <EuiModalHeaderTitle>
+          {t("wormhole_page.token_select_modal.title")}
+        </EuiModalHeaderTitle>
+      </EuiModalHeader>
+      <EuiModalBody className="modalBody">
+        <EuiSelectable
+          options={options}
+          renderOption={renderTokenOption}
+          onChange={onSelectToken}
+          singleSelection
+          searchable
+          height="full"
+          listProps={{
+            rowHeight: 40,
+          }}
+        >
+          {(list, search) => (
+            <>
+              {search}
+              {list}
+            </>
+          )}
+        </EuiSelectable>
+      </EuiModalBody>
+    </CustomModal>
+  );
+};
+
+export {};

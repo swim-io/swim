015649cf88a6cf93e1dc17094ceacf1af053f1b6
wormat <wormat@maelstrom.finance>
2022-10-25 18:18:02 +0200

    fix(ui): Make deserializeLegacySolanaPoolState work

--- apps/ui/src/models/solana/deserializeLegacySolanaPoolState.ts
+++ apps/ui/src/models/solana/deserializeLegacySolanaPoolState.ts
@@ -6,11 +6,13 @@ import {
   struct,
   u128,
   u32,
+  u64,
   u8,
 } from "@project-serum/borsh";
 import type { Layout } from "@project-serum/borsh";
 import type { SolanaEcosystemId, SolanaPoolState } from "@swim-io/solana";
-import { SOLANA_ECOSYSTEM_ID, ampFactor } from "@swim-io/solana";
+import { SOLANA_ECOSYSTEM_ID } from "@swim-io/solana";
+import type BN from "bn.js";
 
 type U8 = (property?: string) => Layout<number>;
 
@@ -29,6 +31,32 @@ export interface LegacySolanaPoolState
   readonly preparedLpFee: number;
   readonly ecosystem: SolanaEcosystemId;
 }
+
+interface DecimalBN {
+  readonly value: BN;
+  readonly decimals: number;
+}
+
+const decimal = (property = "decimal"): Layout<DecimalBN> =>
+  struct([u64("value"), u8("decimals")], property);
+
+interface AmpFactor {
+  readonly initialValue: DecimalBN;
+  readonly initialTs: BN;
+  readonly targetValue: DecimalBN;
+  readonly targetTs: BN;
+}
+
+const ampFactor = (property = "ampFactor"): Layout<AmpFactor> =>
+  struct(
+    [
+      decimal("initialValue"),
+      i64("initialTs"),
+      decimal("targetValue"),
+      i64("targetTs"),
+    ],
+    property,
+  );
 
 export const solanaPool = (
   numberOfTokens: number,

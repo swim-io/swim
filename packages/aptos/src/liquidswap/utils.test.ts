ca3a00328dfa38efc4795c97e5b856ec503cf73d
nicomiicro <104464506+nicomiicro@users.noreply.github.com>
2022-10-26 20:08:34 +0300

    chore(aptos): fix typo
    Co-authored-by: Will <86618292+wormat@users.noreply.github.com>

--- packages/aptos/src/liquidswap/utils.test.ts
+++ packages/aptos/src/liquidswap/utils.test.ts
@@ -5,7 +5,7 @@ describe("utils", () => {
     "0x190d44266241744264b964a37b8f09863167a12d3e70cda39376cfb4e3561e12::liquidity_pool::LiquidityPool<0x30ab37efc691ea7202540b50f3f0f6b090adb143c0746fd49a7e4b7c5870ce8b::coin::T, 0x8c9d3a36ae2c7a765826c126fe625f39e9110ea329a5693d874e875227a889c2::test_coin::USDC, 0x190d44266241744264b964a37b8f09863167a12d3e70cda39376cfb4e3561e12::curves::Uncorrelated>";
 
   describe("getAddLiquidityTransactionPayload", () => {
-    it("should create a AddLiquidity Transaction payload", () => {
+    it("should create an AddLiquidity Transaction payload", () => {
       const coinXAmountAtomic = "1000000";
       const coinYAmountAtomic = "2000000";
       expect(

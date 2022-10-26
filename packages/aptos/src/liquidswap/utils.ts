d3a923a0294706cd982ef539c53f1f348b83dd95
Nico Miicro <nico@swim.io>
2022-10-26 16:20:06 +0300

    fix(aptos): remove buggy liquidswap SDK

--- packages/aptos/src/liquidswap/utils.ts
+++ packages/aptos/src/liquidswap/utils.ts
@@ -1,9 +1,42 @@
-import { isSortedSymbols } from "@pontem/liquidswap-sdk/dist/tsc/utils/contracts.js";
 import type { PoolState, TokenDetails } from "@swim-io/core";
 import { atomicToHuman } from "@swim-io/utils";
 import { Decimal } from "decimal.js";
 
 import type { PoolResource } from "./types";
+
+const EQUAL = 0;
+const LESS_THAN = 1;
+const GREATER_THAN = 2;
+
+function cmp(a: number, b: number) {
+  if (a === b) {
+    return EQUAL;
+  } else if (a < b) {
+    return LESS_THAN;
+  } else {
+    return GREATER_THAN;
+  }
+}
+
+function compare(symbolX: string, symbolY: string) {
+  const lenCmp = cmp(symbolX.length, symbolY.length);
+  if (lenCmp != EQUAL) {
+    return lenCmp;
+  }
+  let i = 0;
+  while (i < symbolX.length && i < symbolY.length) {
+    const elem_cmp = cmp(symbolX.charCodeAt(i), symbolY.charCodeAt(i));
+    if (elem_cmp != EQUAL) return elem_cmp;
+    i++;
+  }
+  return EQUAL;
+}
+
+// Their fixed isSortedSymbols is not published yet...
+// https://github.com/pontem-network/liquidswap-sdk/commit/bc3b715de2556e325bf3d5dfeffa65cd1a59ebfb
+export function isSortedSymbols(symbolX: string, symbolY: string) {
+  return compare(symbolX, symbolY) === LESS_THAN;
+}
 
 export const getPoolBalances = (
   pool: PoolResource,
